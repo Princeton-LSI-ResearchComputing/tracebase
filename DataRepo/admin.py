e2c217e87fbffeadf4b67d2c3a86176ca90a8074
hepcat72 <hepcat72@gmail.com>
2021-03-12 17:51:24 -0500

    Resolves the model for issue #10.
    Files checked in: .gitignore CONTRIBUTING.md DataRepo/admin.py DataRepo/models.py DataRepo/urls.py DataRepo/views.py TraceBase/.env.example TraceBase/settings.py TraceBase/urls.py

--- DataRepo/admin.py
+++ DataRepo/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Compound
+
+@admin.register(Compound)
+class CompoundAdmin(admin.ModelAdmin):
+    list_display = ['name', 'formula', 'hmdb_id']
--- DataRepo/models.py
+++ DataRepo/models.py
@@ -1,3 +1,9 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+class Compound(models.Model):
+    id      = models.AutoField(primary_key=True)
+    name    = models.CharField(max_length=256,unique=True)
+    formula = models.CharField(max_length=256)
+
+    # ID to serve as an external link to record in https://hmdb.ca
+    hmdb_id = models.CharField(max_length=11, blank=True)
