b3355a738a212c6f2cb558b98c17f745d132e16e
hepcat72 <hepcat72@gmail.com>
2021-03-17 15:02:31 -0400

    Linked the HMDB IDs to the hmdb.
    Files checked in: DataRepo/templates/compound_detail.html DataRepo/templates/home.html DataRepo/views.py TraceBase/.env.example TraceBase/settings.py

--- DataRepo/views.py
+++ DataRepo/views.py
@@ -2,11 +2,11 @@ from django.http import Http404
 from django.shortcuts import render
 
 from .models import Compound
-
+from django.conf import settings
 
 def home(request):
     cpds = Compound.objects.all()
-    return render(request, "home.html", {"cpds": cpds})
+    return render(request, "home.html", {"cpds": cpds, "HMDB_CPD_URL": settings.HMDB_CPD_URL})
 
 
 def compound_detail(request, cpd_id):
@@ -14,4 +14,4 @@ def compound_detail(request, cpd_id):
         cpd = Compound.objects.get(id=cpd_id)
     except Compound.DoesNotExist:
         raise Http404("compound not found")
-    return render(request, "compound_detail.html", {"cpd": cpd})
+    return render(request, "compound_detail.html", {"cpd": cpd, "HMDB_CPD_URL": settings.HMDB_CPD_URL})
