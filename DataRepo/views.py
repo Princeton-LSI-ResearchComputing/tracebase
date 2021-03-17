b472277607bc5573e8c29e6c9e211d7abbde9f37
hepcat72 <hepcat72@gmail.com>
2021-03-17 15:13:07 -0400

    Linting after latest changes.
    Files checked in: DataRepo/views.py

--- DataRepo/views.py
+++ DataRepo/views.py
@@ -1,12 +1,15 @@
+from django.conf import settings
 from django.http import Http404
 from django.shortcuts import render
 
 from .models import Compound
-from django.conf import settings
+
 
 def home(request):
     cpds = Compound.objects.all()
-    return render(request, "home.html", {"cpds": cpds, "HMDB_CPD_URL": settings.HMDB_CPD_URL})
+    return render(
+        request, "home.html", {"cpds": cpds, "HMDB_CPD_URL": settings.HMDB_CPD_URL}
+    )
 
 
 def compound_detail(request, cpd_id):
@@ -14,4 +17,8 @@ def compound_detail(request, cpd_id):
         cpd = Compound.objects.get(id=cpd_id)
     except Compound.DoesNotExist:
         raise Http404("compound not found")
-    return render(request, "compound_detail.html", {"cpd": cpd, "HMDB_CPD_URL": settings.HMDB_CPD_URL})
+    return render(
+        request,
+        "compound_detail.html",
+        {"cpd": cpd, "HMDB_CPD_URL": settings.HMDB_CPD_URL},
+    )
