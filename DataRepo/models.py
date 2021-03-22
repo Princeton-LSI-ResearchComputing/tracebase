2888039a5328d65572b65a03f346441b9f00fc79
hepcat72 <hepcat72@gmail.com>
2021-03-22 16:32:55 -0400

    Added Protocol and MSRun models.
    Also created a stub placeholder model for Sample, since MSRun linked to it.
    I made and applied migrations, which incidentall, included a change to hmdb_id from the Compound model, because Lance had modified it to not allow it to be blank.
    
    Files checked in: DataRepo/models.py DataRepo/migrations/0002_auto_20210322_1629.py

--- DataRepo/models.py
+++ DataRepo/models.py
@@ -17,3 +17,27 @@ class Compound(models.Model):
     def hmdb_url(self):
         "Returns the url to the compound's hmdb record"
         return f"{self.HMDB_CPD_URL}/{self.hmdb_id}"
+
+class Protocol(models.Model):
+    # Instance / model fields
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=256, unique=True)
+    description = models.TextField(blank=True)
+
+class Sample(models.Model):
+    ##
+    ## Placeholder to allow creation of class MSRun
+    ##
+
+    # Instance / model fields
+    id = models.AutoField(primary_key=True)
+
+class MSRun(models.Model):
+    # Instance / model fields
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=256, unique=True)
+    date = models.DateField(auto_now=False, auto_now_add=True, editable=True)
+    # Don't allow a Protocol to be deleted if an MSRun links to it
+    protocol_id = models.ForeignKey(Protocol,on_delete=models.RESTRICT)
+    # Don't allow a Sample to be deleted if an MSRun links to it
+    sample_id = models.ForeignKey(Sample,on_delete=models.RESTRICT)
