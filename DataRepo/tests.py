2e7fed4d14f4910981dfcc2da8d77eb9e2bc1bfc
hepcat72 <hepcat72@gmail.com>
2021-03-22 17:23:06 -0400

    Tests, formatting, doc, & field name edits.
    In the python code, it made more sense to name foreign keys by the table name given the way you access them.
    I had to add migrations to the flake8 exclusions or else it complained about long lines in that auto-generated code.
    
    Files checked in: .flake8 CONTRIBUTING.md DataRepo/migrations/0002_auto_20210322_1629.py DataRepo/models.py DataRepo/tests.py DataRepo/migrations/0003_auto_20210322_1710.py

--- DataRepo/tests.py
+++ DataRepo/tests.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from django.test import TestCase
 
-from .models import Compound
+from .models import Compound, MSRun, Protocol, Sample
 
 
 class CompoundTests(TestCase):
@@ -18,3 +20,25 @@ class CompoundTests(TestCase):
         """Compound hmdb url"""
         alanine = Compound.objects.get(name="alanine")
         self.assertEqual(alanine.hmdb_url, f"{Compound.HMDB_CPD_URL}/{alanine.hmdb_id}")
+
+
+class ProtocolTests(TestCase):
+    def setUp(self):
+        Protocol.objects.create(name="p1", description="p1desc")
+
+    def test_protocol_name(self):
+        """Protocol lookup by name"""
+        ptcl = Protocol.objects.get(name="p1")
+        self.assertEqual(ptcl.description, "p1desc")
+
+
+class MSRunTests(TestCase):
+    def setUp(self):
+        p1 = Protocol.objects.create(name="p1", description="p1desc")
+        s1 = Sample.objects.create()
+        MSRun.objects.create(name="msr1", date=datetime.now(), protocol=p1, sample=s1)
+
+    def test_msrun_protocol(self):
+        """MSRun lookup by name"""
+        msr = MSRun.objects.get(name="msr1")
+        self.assertEqual(msr.protocol.id, 1)
