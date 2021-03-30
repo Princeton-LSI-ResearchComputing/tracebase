c143b22ae8cfede767c1274255a5c45a3315d827
John C. Matese <jcmatese@princeton.edu>
2021-03-30 16:05:29 -0400

    Whitespace changes, only ; linters-at-work

--- DataRepo/models.py
+++ DataRepo/models.py
@@ -27,7 +27,7 @@ class Study(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=256, unique=True)
     description = models.TextField(blank=True)
-    
+
     def __str__(self):
         return str(self.name)
 
