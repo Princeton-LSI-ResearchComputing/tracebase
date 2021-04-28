7652a0089b12e7e12c11bf0bf6500f1856a96dff
John C. Matese <jcmatese@princeton.edu>
2021-04-28 17:46:17 -0400

    add a space to help_text

--- DataRepo/migrations/0007_peakdata_validator.py
+++ DataRepo/migrations/0007_peakdata_validator.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name="labeled_count",
             field=models.PositiveSmallIntegerField(
                 blank=True,
-                help_text="the M+ value (i.e. Label) for this observation. "
+                help_text="the M+ value (i.e. Label) for this observation.  "
                 "'1' means one atom is labeled.  '3' means 3 atoms are labeled",
                 null=True,
                 validators=[
