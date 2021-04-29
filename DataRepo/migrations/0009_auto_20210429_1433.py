234d492072251a00c2fd84f85e8776b0b5adfe1e
Lance Parsons <lparsons@princeton.edu>
2021-04-29 14:51:56 -0400

    Fix migrations formatting

--- DataRepo/migrations/0009_auto_20210429_1433.py
+++ DataRepo/migrations/0009_auto_20210429_1433.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('DataRepo', '0008_changed_unique_constraints_coding'),
+        ("DataRepo", "0008_changed_unique_constraints_coding"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='msrun',
-            name='date',
+            model_name="msrun",
+            name="date",
             field=models.DateField(),
         ),
     ]
