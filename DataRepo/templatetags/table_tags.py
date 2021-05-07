309cf6219f2d0f39528b37e5cfecff2315382290
hepcat72 <hepcat72@gmail.com>
2021-05-07 13:49:49 -0400

    Implemented sub-templates.
    Files checked in: DataRepo/templates/listview.html DataRepo/templatetags/table_tags.py DataRepo/templates/Compound/hmdb_id.html

--- DataRepo/templatetags/table_tags.py
+++ DataRepo/templatetags/table_tags.py
@@ -1,7 +1,17 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
 @register.filter
 def value_from_model(model, field):
     return getattr(model, field)
+
+@register.filter
+@stringfilter
+def template_exists(value):
+    try:
+        template.loader.get_template(value)
+        return True
+    except template.TemplateDoesNotExist:
+        return False
