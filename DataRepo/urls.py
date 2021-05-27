0ddaa223f3c22548c5fed7e9868bc9c3ce52ed55
hepcat72 <hepcat72@gmail.com>
2021-05-27 15:34:03 -0400

    Generic Basic Search: proof of concept.
    This commit contains temporary fields - just to make sure I could actually perform searches using all variables.
    
    This commit also contains some stuff I copied from Fan's fkt2 branch (which I modified just to get a basic working implementation.  I expect that all to be replaced with Fan's final views version.
    
    Files checked in: DataRepo/migrations/0001_reinitialize.py DataRepo/migrations/0002_sample_time_collected.py DataRepo/migrations/0003_AnimalTreatment.py DataRepo/templates/home.html DataRepo/urls.py DataRepo/views.py DataRepo/migrations/0001_initial.py DataRepo/templates/DataRepo/study_detail.html DataRepo/templates/DataRepo/study_list.html DataRepo/templates/peakgroups_results.html

--- DataRepo/urls.py
+++ DataRepo/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("compound/", views.compound_list, name="compound_list"),
     path("compound/<int:cpd_id>/", views.compound_detail, name="compound_detail"),
+    path("study/", views.StudyListView.as_view(), name="study_list"),
+    path("study/<int:pk>/", views.StudyDetailView.as_view(), name="study_detail"),
+    path("search_basic/<str:mdl>/<str:fld>/<str:cmp>/<str:val>/<str:fmt>/",
+        views.search_basic, name="search_basic"),
 ]
--- DataRepo/views.py
+++ DataRepo/views.py
@@ -1,7 +1,10 @@
 from django.http import Http404
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView, TemplateView
+from django.apps import AppConfig
+from django.apps import apps
 
-from .models import Compound
+from .models import Compound, Study
 
 
 def home(request):
@@ -19,3 +22,42 @@ def compound_detail(request, cpd_id):
     except Compound.DoesNotExist:
         raise Http404("compound not found")
     return render(request, "compound_detail.html", {"cpd": cpd})
+
+
+class StudyListView(ListView):
+    """Generic class-based view for a list of studies."""
+    model = Study
+    paginate_by = 20
+
+
+class StudyDetailView(DetailView):
+    """Generic class-based detail view for a study."""
+    model = Study
+
+
+def search_basic(request, mdl, fld, cmp, val, fmt):
+    """Generic basic search interface"""
+    model = apps.get_app_config('DataRepo').get_model(mdl) #works
+    field = fld
+    comparator = cmp
+    value = val
+    format_name = fmt
+
+    qry = {}
+    qry["mdl"] = model.__name__
+    qry["fld"] = field
+    qry["cmp"] = comparator
+    qry["val"] = value
+    qry["fmt"] = format_name
+
+    # https://stackoverflow.com/questions/4720079/django-query-filter-with-variable-column
+    fld_cmp = field + '__' + comparator
+    qs = [model.objects.get(**{ fld_cmp: value })]
+
+    format_template = ""
+    if fmt == "peakgroups":
+        format_template = "peakgroups_results.html"
+    else:
+        raise Http404("Results format [" + fmt + "] page not found")
+    
+    return render(request, format_template, {"qry": qry, "qs": qs})
