93329fa0a85e90ce68bef21000f105e25afa1f4c
hepcat72 <hepcat72@gmail.com>
2021-05-28 15:37:39 -0400

    Added Animal Fields.
    Files checked in: DataRepo/migrations/0001_initial.py DataRepo/templates/peakgroups_results.html DataRepo/urls.py DataRepo/views.py

--- DataRepo/urls.py
+++ DataRepo/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path("compound/<int:cpd_id>/", views.compound_detail, name="compound_detail"),
     path("study/", views.StudyListView.as_view(), name="study_list"),
     path("study/<int:pk>/", views.StudyDetailView.as_view(), name="study_detail"),
-    path("search_basic/<str:mdl>/<str:fld>/<str:cmp>/<str:val>/<str:fmt>/",
-        views.search_basic, name="search_basic"),
+    path(
+        "search_basic/<str:mdl>/<str:fld>/<str:cmp>/<str:val>/<str:fmt>/",
+        views.search_basic,
+        name="search_basic",
+    ),
 ]
