5f14e5039b83ce75a5289e548cbcf4d3919932e5
fkang-pu <fkang@princeton.edu>
2021-06-02 11:07:33 -0400

    commit changes to html files in DataRepo/templates and related urls and views for Compound and Study

--- DataRepo/urls.py
+++ DataRepo/urls.py
@@ -4,6 +4,14 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("compound/", views.compound_list, name="compound_list"),
-    path("compound/<int:cpd_id>/", views.compound_detail, name="compound_detail"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    # path("compound/", views.compound_list, name="compound_list"),
+    # path("compound/<int:cpd_id>/", views.compound_detail, name="compound_detail"
+    # path for each CBV
+    path("compounds/", views.CompoundListView.as_view(), name="compound_list"),
+    path(
+        "compounds/<int:pk>/", views.CompoundDetailView.as_view(), name="compound_detail"
+    ),
+    path("studies/", views.StudyListView.as_view(), name="study_list"),
+    path("studies/<int:pk>/", views.StudyDetailView.as_view(), name="study_detail"),
 ]
--- DataRepo/views.py
+++ DataRepo/views.py
@@ -1,12 +1,24 @@
-from django.http import Http404
+# from django.http import Http404
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-from .models import Compound
+from .models import (
+    Compound,
+    Study
+)
 
 
 def home(request):
     return render(request, "home.html")
 
+
+def dashboard(request):
+    return render(request, "DataRepo/dashboard.html")
+
+
+"""
+For Compound list or detail:
+replace Function-based view by Class-based view (CBV)
 
 def compound_list(request):
     cpds = Compound.objects.all()
@@ -19,3 +31,37 @@ def compound_detail(request, cpd_id):
     except Compound.DoesNotExist:
         raise Http404("compound not found")
     return render(request, "compound_detail.html", {"cpd": cpd})
+"""
+
+"""
+For better readability:
+    list template name for each ListView or DetailView
+    list context_object_name for each ListView
+"""
+
+# Generic class-based view for a list of compounds
+class CompoundListView(ListView):  
+    model = Compound
+    context_object_name = "compound_list"
+    template_name = "DataRepo/compound_list.html"
+    paginate_by = 20
+
+
+# Generic class-based detail view for a compound
+class CompoundDetailView(DetailView): 
+    model = Compound
+    template_name = "DataRepo/compound_detail.html"
+
+
+# Generic class-based view for a list of studies
+class StudyListView(ListView):
+    model = Study
+    context_object_name = "study_list"
+    template_name = "DataRepo/study_list.html"
+    paginate_by = 20
+
+
+# Generic class-based detail view for a study
+class StudyDetailView(DetailView):
+    model = Study
+    template_name = "DataRepo/study_detail.html"
