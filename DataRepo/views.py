81b0b5d3588248bd9a40eae056917728b02fa508
hepcat72 <hepcat72@gmail.com>
2021-06-03 18:07:55 -0400

    This works for Study, Animal, and Sample.  Animal and Sample are really fast, but Study takes about a minute and a half.  So I'm wondering if there's a way to possibly make the related records be included, but appear as separate variables.
    Files checked in: DataRepo/templates/peakgroups_results.html DataRepo/views.py

--- DataRepo/views.py
+++ DataRepo/views.py
@@ -3,8 +3,10 @@ from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 from django.db.models import Prefetch
+from django.db.models import prefetch_related_objects
+from django.db.models import FilteredRelation, Q
 
-from .models import Compound, Study, Tissue, MSRun, PeakData
+from .models import Compound, Study, Tissue, MSRun, PeakGroup, PeakData, Sample, Animal
 
 
 def home(request):
@@ -48,25 +50,70 @@ def search_basic(request, mdl, fld, cmp, val, fmt):
     qry["val"] = val
     qry["fmt"] = fmt
 
-    # https://stackoverflow.com/questions/4720079/django-query-filter-with-variable-column
-    fld_cmp = fld + "__" + cmp
-
     format_template = ""
     if fmt == "peakgroups":
         format_template = "peakgroups_results.html"
 
-        # https://docs.djangoproject.com/en/3.2/topics/db/queries/#following-relationships-backward
-        #studies = model.objects.filter(**{fld_cmp: val}).prefetch_related("animals","animals__samples__tissue","animals__samples__msruns","animals__samples__msruns__peak_groups","animals__samples__msruns__peak_groups__peak_data")
+        if mdl == "Study":
+            # https://stackoverflow.com/questions/4720079/django-query-filter-with-variable-column
+            fld_cmp = fld + "__" + cmp
 
-        # This actually causes roughly 10% *more* queries
-        # https://stackoverflow.com/questions/29358729/prefetch-object-with-multiple-levels-of-reverse-lookups/29381638
-        studies = model.objects.filter(**{fld_cmp: val}).select_related().prefetch_related(
-            "animals",
-            Prefetch("animals__samples__tissue",queryset=Tissue.objects.all(),to_attr="tissue_list"),
-            Prefetch("animals__samples__msruns",queryset=MSRun.objects.all(),to_attr="msr_list"),
-            Prefetch("animals__samples__msruns__peak_groups__peak_data",queryset=PeakData.objects.all(),to_attr="pd_list"))
+            # https://docs.djangoproject.com/en/3.2/topics/db/queries/#following-relationships-backward
+            studies = Study.objects.filter(**{fld_cmp: val})
+            animals = Animal.objects.filter(studies__in=studies)
+            samples = Sample.objects.filter(animal__in=animals)
+        elif mdl == "Animal":
+            fld_cmp = fld + "__" + cmp
+            study_field_cmp = "animals__" + fld_cmp
 
-        res = render(request, format_template, {"qry": qry, "studies": studies})
+            # https://stackoverflow.com/questions/29358729/prefetch-object-with-multiple-levels-of-reverse-lookups/29381638
+            animals = Animal.objects.filter(**{fld_cmp: val})
+            studies = Study.objects.filter(**{study_field_cmp: val})
+            samples = Sample.objects.filter(animal__in=animals)
+        elif mdl == "Sample":
+            fld_cmp = fld + "__" + cmp
+            animal_field_cmp = "samples__" + fld_cmp
+            study_field_cmp = "animals__" + animal_field_cmp
+
+            samples = Sample.objects.filter(**{fld_cmp: val})
+            animals = Animal.objects.filter(**{animal_field_cmp: val})
+            studies = Study.objects.filter(animals__in=animals)
+            #studies = Study.objects.all().prefetch_related(
+            #    Prefetch("animals__samples",queryset=Sample.objects.filter(**{fld_cmp: val}),to_attr="sample_list"),
+            #    "animals__samples__tissue",
+            #    "animals__samples__msruns",
+            #    "animals__samples__msruns__peak_groups__peak_data")
+        elif mdl == "Tissue":
+            fld_cmp = "tissues__" + fld + "__" + cmp
+
+            studies = Study.objects.all().prefetch_related(
+                Prefetch("animals__samples__tissue",queryset=Tissue.objects.filter(**{fld_cmp: val}),to_attr="tissue_list"),
+                "animals__samples__msruns",
+                "animals__samples__msruns__peak_groups__peak_data")
+        elif mdl == "MSRun":
+            fld_cmp = "msruns__" + fld + "__" + cmp
+
+            studies = Study.objects.all().prefetch_related(
+                "animals__samples__tissue",
+                Prefetch("animals__samples__msruns",queryset=MSRun.objects.filter(**{fld_cmp: val}),to_attr="msrun_list"),
+                "animals__samples__msruns__peak_groups__peak_data")
+        elif mdl == "PeakGroup":
+            fld_cmp = "peak_groups__" + fld + "__" + cmp
+
+            studies = Study.objects.all().prefetch_related(
+                "animals__samples__tissue",
+                "animals__samples__msruns",
+                Prefetch("animals__samples__msruns__peak_groups",queryset=PeakGroup.objects.filter(**{fld_cmp: val}),to_attr="peakgroup_list"),
+                "animals__samples__msruns__peak_groups__peak_data")
+        elif mdl == "PeakData":
+            fld_cmp = "peak_data__" + fld + "__" + cmp
+
+            studies = Study.objects.all().prefetch_related(
+                "animals__samples__tissue",
+                "animals__samples__msruns",
+                Prefetch("animals__samples__msruns__peak_groups__peak_data",queryset=PeakData.objects.filter(**{fld_cmp: val}),to_attr="peakgroup_list"))
+
+        res = render(request, format_template, {"qry": qry, "studies": studies, "animals":animals, "samples": samples})
     else:
         raise Http404("Results format [" + fmt + "] page not found")
 
