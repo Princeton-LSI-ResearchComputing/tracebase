99c170ce36ff1a3aecf339b1af584be43522e296
hepcat72 <hepcat72@gmail.com>
2021-06-04 13:35:05 -0400

    Finally a generic basic search that works.
    I was doing it backwards before.  You have the start from the PeakData table and then you only need 1 nested loop on the M:M relationship between Animal and Study.
    
    Files checked in: DataRepo/templates/peakgroups_results.html DataRepo/views.py

--- DataRepo/views.py
+++ DataRepo/views.py
@@ -2,11 +2,9 @@ from django.apps import apps
 from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
-from django.db.models import Prefetch
-from django.db.models import prefetch_related_objects
-from django.db.models import FilteredRelation, Q
+from django.core.exceptions import FieldError
 
-from .models import Compound, Study, Tissue, MSRun, PeakGroup, PeakData, Sample, Animal
+from .models import Compound, Study, PeakData
 
 
 def home(request):
@@ -53,67 +51,31 @@ def search_basic(request, mdl, fld, cmp, val, fmt):
     format_template = ""
     if fmt == "peakgroups":
         format_template = "peakgroups_results.html"
+        fld_cmp = ""
 
         if mdl == "Study":
-            # https://stackoverflow.com/questions/4720079/django-query-filter-with-variable-column
-            fld_cmp = fld + "__" + cmp
-
-            # https://docs.djangoproject.com/en/3.2/topics/db/queries/#following-relationships-backward
-            studies = Study.objects.filter(**{fld_cmp: val})
-            animals = Animal.objects.filter(studies__in=studies)
-            samples = Sample.objects.filter(animal__in=animals)
+            fld_cmp = "peak_group__ms_run__sample__animal__studies__"
         elif mdl == "Animal":
-            fld_cmp = fld + "__" + cmp
-            study_field_cmp = "animals__" + fld_cmp
-
-            # https://stackoverflow.com/questions/29358729/prefetch-object-with-multiple-levels-of-reverse-lookups/29381638
-            animals = Animal.objects.filter(**{fld_cmp: val})
-            studies = Study.objects.filter(**{study_field_cmp: val})
-            samples = Sample.objects.filter(animal__in=animals)
+            fld_cmp = "peak_group__ms_run__sample__animal__"
         elif mdl == "Sample":
-            fld_cmp = fld + "__" + cmp
-            animal_field_cmp = "samples__" + fld_cmp
-            study_field_cmp = "animals__" + animal_field_cmp
-
-            samples = Sample.objects.filter(**{fld_cmp: val})
-            animals = Animal.objects.filter(**{animal_field_cmp: val})
-            studies = Study.objects.filter(animals__in=animals)
-            #studies = Study.objects.all().prefetch_related(
-            #    Prefetch("animals__samples",queryset=Sample.objects.filter(**{fld_cmp: val}),to_attr="sample_list"),
-            #    "animals__samples__tissue",
-            #    "animals__samples__msruns",
-            #    "animals__samples__msruns__peak_groups__peak_data")
+            fld_cmp = "peak_group__ms_run__sample__"
         elif mdl == "Tissue":
-            fld_cmp = "tissues__" + fld + "__" + cmp
-
-            studies = Study.objects.all().prefetch_related(
-                Prefetch("animals__samples__tissue",queryset=Tissue.objects.filter(**{fld_cmp: val}),to_attr="tissue_list"),
-                "animals__samples__msruns",
-                "animals__samples__msruns__peak_groups__peak_data")
+            fld_cmp = "peak_group__ms_run__sample__tissue__"
         elif mdl == "MSRun":
-            fld_cmp = "msruns__" + fld + "__" + cmp
-
-            studies = Study.objects.all().prefetch_related(
-                "animals__samples__tissue",
-                Prefetch("animals__samples__msruns",queryset=MSRun.objects.filter(**{fld_cmp: val}),to_attr="msrun_list"),
-                "animals__samples__msruns__peak_groups__peak_data")
+            fld_cmp = "peak_group__ms_run__"
         elif mdl == "PeakGroup":
-            fld_cmp = "peak_groups__" + fld + "__" + cmp
+            fld_cmp = "peak_group__"
+        elif mdl != "PeakData":
+            raise Http404("Table [" + mdl + "] is not searchable in the [" + fmt + "] results format")
 
-            studies = Study.objects.all().prefetch_related(
-                "animals__samples__tissue",
-                "animals__samples__msruns",
-                Prefetch("animals__samples__msruns__peak_groups",queryset=PeakGroup.objects.filter(**{fld_cmp: val}),to_attr="peakgroup_list"),
-                "animals__samples__msruns__peak_groups__peak_data")
-        elif mdl == "PeakData":
-            fld_cmp = "peak_data__" + fld + "__" + cmp
+        fld_cmp += fld + "__" + cmp
 
-            studies = Study.objects.all().prefetch_related(
-                "animals__samples__tissue",
-                "animals__samples__msruns",
-                Prefetch("animals__samples__msruns__peak_groups__peak_data",queryset=PeakData.objects.filter(**{fld_cmp: val}),to_attr="peakgroup_list"))
+        try:
+            peakdata = PeakData.objects.filter(**{fld_cmp: val})
+        except FieldError as fe:
+            raise Http404("Table [" + mdl + "] either does not contain a field named [" + fld + "] or that field is not searchable.  Note, none of the cached property fields are searchable.  The error was: " + str(fe))
 
-        res = render(request, format_template, {"qry": qry, "studies": studies, "animals":animals, "samples": samples})
+        res = render(request, format_template, {"qry": qry, "pds": peakdata})
     else:
         raise Http404("Results format [" + fmt + "] page not found")
 
