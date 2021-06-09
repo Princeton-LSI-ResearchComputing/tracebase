9f4a8e3afbf50b66fff1d50846d067a9fe9c6e97
fkang-pu <fkang@princeton.edu>
2021-06-09 09:50:20 -0400

    add views and templates for 7 models

--- DataRepo/urls.py
+++ DataRepo/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -16,5 +16,47 @@ urlpatterns = [
         "search_basic/<str:mdl>/<str:fld>/<str:cmp>/<str:val>/<str:fmt>/",
         views.search_basic,
         name="search_basic",
+    ),
+    path("protocols/", views.ProtocolListView.as_view(), name="protocol_list"),
+    path(
+        "protocols/<int:pk>/",
+        views.ProtocolDetailView.as_view(),
+        name="protocol_detail",
+    ),
+    path("animals/", views.AnimalListView.as_view(), name="animal_list"),
+    path("animals/<int:pk>/", views.AnimalDetailView.as_view(), name="animal_detail"),
+    path("samples/", views.SampleListView.as_view(), name="sample_list"),
+    re_path(
+        r"^samples/(?P<animal_id>\d+$)/",
+        views.SampleListView.as_view(),
+        name="sample_list",
+    ),
+    path("samples/<int:pk>/", views.SampleDetailView.as_view(), name="sample_detail"),
+    path("msruns/", views.MSRunListView.as_view(), name="msrun_list"),
+    path("msruns/<int:pk>/", views.MSRunDetailView.as_view(), name="msrun_detail"),
+    path(
+        "peakgroupsets/", views.PeakGroupSetListView.as_view(), name="peakgroupset_list"
+    ),
+    path(
+        "peakgroupsets/<int:pk>/",
+        views.PeakGroupSetDetailView.as_view(),
+        name="peakgroupset_detail",
+    ),
+    path("peakgroups/", views.PeakGroupListView.as_view(), name="peakgroup_list"),
+    re_path(
+        r"^peakgroups/(?P<ms_run_id>\d+$)/",
+        views.PeakGroupListView.as_view(),
+        name="peakgroup_list",
+    ),
+    path(
+        "peakgroups/<int:pk>/",
+        views.PeakGroupDetailView.as_view(),
+        name="peakgroup_detail",
+    ),
+    path("peakdata/", views.PeakDataListView.as_view(), name="peakdata_list"),
+    re_path(
+        r"^peakdata/(?P<peak_group_id>\d+$)/",
+        views.PeakDataListView.as_view(),
+        name="peakdata_list",
     ),
 ]
