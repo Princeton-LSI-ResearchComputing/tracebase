12f9403b3937ab036591812d5cc3182ad087c798
hepcat72 <hepcat72@gmail.com>
2021-06-17 14:16:50 -0400

    Show all records to start out.
    There are 2 problems with this thus far:
    
    1. If you make an invalid search, the currently displayed data diappears.
    2. If you remove all search criteria and search to get back to "all" data, you can no longer add search criteia (i.e. rows)
    
    Files checked in: DataRepo/templates/DataRepo/peakgroups_results2.html DataRepo/views.py DataRepo/templatetags/advsrch_tags.py

---
+++ DataRepo/templatetags/advsrch_tags.py
@@ -0,0 +1,16 @@
+from django import template
+from DataRepo.views import getAllPeakGroupsFmtData
+
+register = template.Library()
+
+@register.tag(name="get_all_peakgroups")
+def do_get_all_peakgroups():
+    return getAllPeakGroupsFmtData()
+
+@register.simple_tag
+def define(the_val):
+    return the_val
+
+@register.simple_tag
+def populatePeakGroups(queryset):
+    return getAllPeakGroupsFmtData()
