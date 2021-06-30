209d8446cbacd028928aa5e94c5b0e55674ccd87
hepcat72 <hepcat72@gmail.com>
2021-06-30 17:33:27 -0400

    Removed vestigial tag from advsrch_tags.py.
    Files checked in: DataRepo/templatetags/advsrch_tags.py

--- DataRepo/templatetags/advsrch_tags.py
+++ DataRepo/templatetags/advsrch_tags.py
@@ -5,11 +5,6 @@ from DataRepo.views import getAllPeakGroupsFmtData
 register = template.Library()
 
 
-@register.tag(name="get_all_peakgroups")
-def do_get_all_peakgroups():
-    return getAllPeakGroupsFmtData()
-
-
 @register.simple_tag
 def define(the_val):
     return the_val
