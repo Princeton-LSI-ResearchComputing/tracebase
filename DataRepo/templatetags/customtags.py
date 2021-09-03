efd1aad7ff4e7b858cd752e3db79f201bae1150f
hepcat72 <hepcat72@gmail.com>
2021-09-03 17:20:26 -0400

    Linting wanted multiple hard returns between tag filters.
    Files checked in: DataRepo/templatetags/customtags.py

--- DataRepo/templatetags/customtags.py
+++ DataRepo/templatetags/customtags.py
@@ -11,17 +11,20 @@ def getFormatName(qry, fmt):
     """
     return qry["searches"][fmt]["name"]
 
+
 @register.filter
 def durationToWeeks(td):
     if td is None:
         return None
     return td.total_seconds() // 604800
 
+
 @register.filter
 def decimalPlaces(number, places):
     if number is None:
         return None
     return floatformat(number, places)
+
 
 # This allows indexing a list or dict
 @register.filter
