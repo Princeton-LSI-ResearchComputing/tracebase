caab766efe59a13cc38714d1fe2641459ddf10df
hepcat72 <hepcat72@gmail.com>
2021-09-03 15:58:12 -0400

    Ensured that age would be presented in weeks.
    Files checked in: DataRepo/templates/DataRepo/search/downloads/peakdata.tsv DataRepo/templates/DataRepo/search/downloads/peakgroups.tsv DataRepo/templates/DataRepo/search/results/peakdata.html DataRepo/templates/DataRepo/search/results/peakgroups.html DataRepo/templatetags/customtags.py

--- DataRepo/templatetags/customtags.py
+++ DataRepo/templatetags/customtags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.defaultfilters import floatformat
 
 register = template.Library()
 
@@ -9,3 +10,17 @@ def getFormatName(qry, fmt):
     Retrieves a format name, given a format
     """
     return qry["searches"][fmt]["name"]
+
+
+@register.filter
+def durationToWeeks(td):
+    if td is None:
+        return None
+    return td.total_seconds() // 604800
+
+
+@register.filter
+def decimalPlaces(number, places):
+    if number is None:
+        return None
+    return floatformat(number, places)
