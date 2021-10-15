4ce4a9468055bdc4d4865e4449b3289c85f2e07a
John C. Matese <jcmatese@princeton.edu>
2021-10-15 13:12:22 -0400

    black and flake8 complaints

--- DataRepo/tests/test_models.py
+++ DataRepo/tests/test_models.py
@@ -433,10 +433,17 @@ class DataLoadingTests(TestCase):
         self.assertEqual(
             PeakGroupSet.objects.all().count(), self.ALL_PEAKGROUPSETS_COUNT
         )
-        pgs = PeakGroupSet.objects.all().order_by("filename").first()
-        self.assertTrue(PeakGroupSet.objects.filter(filename="obob_maven_6eaas_inf.xlsx").exists())
-        self.assertTrue(PeakGroupSet.objects.filter(filename="obob_maven_6eaas_serum.xlsx").exists())
-        self.assertTrue(PeakGroupSet.objects.filter(filename="obob_maven_6eaas_inf_corrected.csv").exists())
+        self.assertTrue(
+            PeakGroupSet.objects.filter(filename="obob_maven_6eaas_inf.xlsx").exists()
+        )
+        self.assertTrue(
+            PeakGroupSet.objects.filter(filename="obob_maven_6eaas_serum.xlsx").exists()
+        )
+        self.assertTrue(
+            PeakGroupSet.objects.filter(
+                filename="obob_maven_6eaas_inf_corrected.csv"
+            ).exists()
+        )
 
     def test_peak_groups_multiple_compounds(self):
         """
