cac7187866396c7b692b7b7e0730d02c1dbbd9e3
fkang-pu <fkang@princeton.edu>
2021-11-15 16:33:25 -0500

    modified customtags.py

--- DataRepo/templatetags/customtags.py
+++ DataRepo/templatetags/customtags.py
@@ -1,7 +1,5 @@
 from django import template
 from django.template.defaultfilters import floatformat
-from django.urls import reverse
-from django.utils.html import format_html_join
 
 from DataRepo.views import getDownloadQryList, manyToManyFilter
 
