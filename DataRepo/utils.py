e16f2240f449e4e3032eb2c66e38d91eb15e348a
John C. Matese <jcmatese@princeton.edu>
2021-11-18 16:05:47 -0500

    Added back type: ignore comment

--- DataRepo/utils.py
+++ DataRepo/utils.py
@@ -4,7 +4,7 @@ import re
 from collections import namedtuple
 from datetime import datetime, timedelta
 
-import dateutil.parser
+import dateutil.parser  # type: ignore
 import pandas as pd
 from django.core.exceptions import ValidationError  # type: ignore
 from django.db import transaction
