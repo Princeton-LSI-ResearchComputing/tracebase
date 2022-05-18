2f0ebf2339e9f6bece5d2be35ad54731083397e8
Lance Parsons <lparsons@princeton.edu>
2022-05-18 16:14:01 -0400

    Use "construction" image for navbar in development (#433)
    In order to make development sites easier to distinguish from
    production, a "construction" background image is used in the navbar.
    
    Resolves #432

--- TraceBase/settings.py
+++ TraceBase/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "DataRepo.context_processors.debug",
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
