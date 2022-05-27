66da333eb8755f95f51fe04ded84c9ca40ff8920
hepcat72 <hepcat72@gmail.com>
2022-05-27 18:36:06 -0400

    Added validation methods for tracer_group_name values.
    Files checked in: DataRepo/models/infusate.py

--- DataRepo/models/infusate.py
+++ DataRepo/models/infusate.py
@@ -74,3 +74,80 @@ class Infusate(MaintainedModel):
             )
             + "}"
         )
+
+    @classmethod
+    def validate_all_tracer_group_names(cls):
+        """
+        This raises an exception if the tracer_group_names are not consistent across infusate records, i.e. they refer
+        to different groups of tracers.
+        """
+        grouped_recs = cls.objects.filter(tracer_group_name__isnull=False)
+        cls.validate_tracer_group_names_helper(grouped_recs, {})
+
+    @classmethod
+    def validate_one_tracer_group_name(cls, name):
+        """
+        This raises an exception if the supplied tracer_group_name is not consistent across infusate records, i.e. they
+        refer to different groups of tracers.
+        """
+        grouped_recs = cls.objects.filter(tracer_group_name__iexact=name)
+        cls.validate_tracer_group_names_helper(grouped_recs, {})
+
+    @classmethod
+    def validate_tracer_group_names_helper(cls, grouped_recs, group_map_dict):
+        for group_rec in grouped_recs:
+            grp_name = group_rec.tracer_group_name
+            if grp_name not in group_map_dict:
+                group_map_dict[grp_name] = {}
+            tracer_key = ",".join(
+                sorted(map(lambda r: str(r.id), group_rec.tracers.all()))
+            )
+            if tracer_key not in group_map_dict[grp_name]:
+                group_map_dict[grp_name][tracer_key] = []
+            group_map_dict[grp_name][tracer_key].append(group_rec.id)
+
+        problems = []
+        for grp_name in group_map_dict:
+            if len(group_map_dict[grp_name].keys()) != 1:
+                num_groupings = len(group_map_dict[grp_name].keys())
+                stats = "\n"
+                i = 0
+                for tracer_key in sorted(
+                    group_map_dict[grp_name],
+                    key=lambda x: len(group_map_dict[grp_name][x]),
+                    reverse=True,
+                ):
+                    stats += (
+                        f"Tracer IDs: {tracer_key} are refered to by {len(group_map_dict[grp_name][tracer_key])} "
+                        "infusate records"
+                    )
+                    i += 1
+                    if i == num_groupings:
+                        stats += (
+                            f", this last one with the following IDs: "
+                            f"{','.join(group_map_dict[grp_name][tracer_key])}"
+                        )
+                    else:
+                        stats += "\n"
+                msg = (
+                    f"Tracer group name {grp_name} is inconsistent.  There are {num_groupings} different groupings of "
+                    "different tracer records.  This group name refers to the following list of tracer IDs by the "
+                    f"indicated number of infusate records: {stats}"
+                )
+                problems.append(msg)
+        if len(problems) > 0:
+            raise InvalidTracerGroupNames(problems)
+
+    def validate_my_tracer_group_name(self):
+        grp_name = self.tracer_group_name
+        grouped_recs = self.objects.filter(tracer_group_name__iexact=grp_name).filter(
+            tracer_group_name__isnull=False
+        )
+        group_map_dict = {grp_name: {}}
+        self.validate_tracer_group_names_helper(grouped_recs, group_map_dict)
+
+
+class InvalidTracerGroupNames(Exception):
+    def __init__(self, messages):
+        message = "\n".join(messages)
+        super().__init__(message)
