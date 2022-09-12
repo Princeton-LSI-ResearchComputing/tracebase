3317b3cf9672c928977fb224884a5936cc347cb2
hepcat72 <hepcat72@gmail.com>
2022-09-12 16:40:52 -0400

    Made a factory for custom test classes to avoid JSCPD linting errors.
    Files checked in: DataRepo/tests/tracebase_test_case.py

--- DataRepo/tests/tracebase_test_case.py
+++ DataRepo/tests/tracebase_test_case.py
@@ -6,99 +6,66 @@ LONG_TEST_THRESH_SECS = 20
 LONG_TEST_ALERT_STR = f" [ALERT > {LONG_TEST_THRESH_SECS}]"
 
 
-class TracebaseTestCase(TestCase):
+def test_case_class_factory(base_class):
     """
-    This wrapper of TestCase makes the necessary/desirable settings for all test classes.
-    """
-
-    maxDiff = None
-    databases = "__all__"
-    classStartTime = time.time()
-
-    def setUp(self):
-        """
-        This method in the superclass is intended to record the start time so that the test run time can be reported in
-        tearDown.
-        """
-        self.testStartTime = time.time()
-
-    def tearDown(self):
-        """
-        This method in the superclass is intended to provide run time information for each test.
-        """
-        self.reportRunTime(self.id(), self.testStartTime)
-
-    def setUpTestData(self):
-        """
-        This method in the superclass is intended to provide run time information for the setUpTestData method.
-        """
-        self.reportRunTime(f"{self.__class__.__name__}.setUpTestData", self.classStartTime)
-
-    def reportRunTime(self, id, startTime):
-        """
-        Print the runtime of a test given the test ID and start time.
-        """
-
-        # TODO: When issue #480 is implemented, a test should be added here to ensure each test runs in under some
-        #       reasonable threshold.  There might even be a way to ensure the data setup runs quickly as well.
-
-        t = time.time() - startTime
-        heads_up = ""  # String to include for tests that run too long
-
-        if t > LONG_TEST_THRESH_SECS:
-            # Add a string that can be easily searched in the terminal to find long running tests.
-            heads_up = LONG_TEST_ALERT_STR
-
-        print("TEST TIME%s: %s: %.3f" % (heads_up, id, t))
-
-    class Meta:
-        abstract = True
-
-
-class TracebaseTestCase(TransactionTestCase):
-    """
-    This wrapper of TestCase makes the necessary/desirable settings for all test classes.
+    Class creation factory where the base class is an argument.  Note, it must receive a TestCase-compatible class.
     """
 
-    maxDiff = None
-    databases = "__all__"
-    classStartTime = time.time()
-
-    def setUp(self):
+    class TracebaseTestCaseTemplate(base_class):
         """
-        This method in the superclass is intended to record the start time so that the test run time can be reported in
-        tearDown.
-        """
-        self.testStartTime = time.time()
-
-    def tearDown(self):
-        """
-        This method in the superclass is intended to provide run time information for each test.
-        """
-        self.reportRunTime(self.id(), self.testStartTime)
-
-    def setUpTestData(self):
-        """
-        This method in the superclass is intended to provide run time information for the setUpTestData method.
-        """
-        self.reportRunTime(f"{self.__class__.__name__}.setUpTestData", self.classStartTime)
-
-    def reportRunTime(self, id, startTime):
-        """
-        Print the runtime of a test given the test ID and start time.
+        This wrapper of both TestCase and TransactionTestCase makes the necessary/desirable settings for all test
+        classes and implements running time reporting.
         """
 
-        # TODO: When issue #480 is implemented, a test should be added here to ensure each test runs in under some
-        #       reasonable threshold.  There might even be a way to ensure the data setup runs quickly as well.
+        maxDiff = None
+        databases = "__all__"
+        classStartTime = time.time()
 
-        t = time.time() - startTime
-        heads_up = ""  # String to include for tests that run too long
+        def setUp(self):
+            """
+            This method in the superclass is intended to record the start time so that the test run time can be
+            reported in tearDown.
+            """
+            self.testStartTime = time.time()
 
-        if t > LONG_TEST_THRESH_SECS:
-            # Add a string that can be easily searched in the terminal to find long running tests.
-            heads_up = LONG_TEST_ALERT_STR
+        def tearDown(self):
+            """
+            This method in the superclass is intended to provide run time information for each test.
+            """
+            reportRunTime(self.id(), self.testStartTime)
 
-        print("TEST TIME%s: %s: %.3f" % (heads_up, id, t))
+        @classmethod
+        def setUpTestData(self):
+            """
+            This method in the superclass is intended to provide run time information for the setUpTestData method.
+            """
+            super().setUpTestData()
+            reportRunTime(f"{self.__name__}.setUpTestData", self.classStartTime)
 
-    class Meta:
-        abstract = True
+        class Meta:
+            abstract = True
+
+    return TracebaseTestCaseTemplate
+
+
+def reportRunTime(id, startTime):
+    """
+    Print the runtime of a test given the test ID and start time.
+    """
+
+    # TODO: When issue #480 is implemented, a test should be added here to ensure each test runs in under some
+    #       reasonable threshold.  There might even be a way to ensure the data setup runs quickly as well.
+
+    t = time.time() - startTime
+    heads_up = ""  # String to include for tests that run too long
+
+    if t > LONG_TEST_THRESH_SECS:
+        # Add a string that can be easily searched in the terminal to find long running tests.
+        heads_up = LONG_TEST_ALERT_STR
+
+    print("TEST TIME%s: %s: %.3f" % (heads_up, id, t))
+
+
+# Classes created by the factory with different base classes:
+TracebaseTestCase = test_case_class_factory(TestCase)
+TracebaseTransactionTestCase = test_case_class_factory(TransactionTestCase)
