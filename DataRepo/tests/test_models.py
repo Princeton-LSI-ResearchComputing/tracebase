3e3b5bec0e6ed91d0f91f488354054d70354cb68
hepcat72 <hepcat72@gmail.com>
2022-09-13 16:44:09 -0400

    Migrations were broken and linting performed.
    Files checked in: DataRepo/migrations/0001_initial.py DataRepo/migrations/0003_auto_20220822_1637.py DataRepo/tests/test_models.py

--- DataRepo/tests/test_models.py
+++ DataRepo/tests/test_models.py
@@ -180,7 +180,8 @@ class StudyTests(TracebaseTestCase, ExampleDataConsumer):
     def test_animal(self):
         self.assertEqual(self.animal.name, self.first["Animal ID"])
         self.assertEqual(
-            self.animal.infusate.tracers.first().compound.name, self.first["Tracer Compound"]
+            self.animal.infusate.tracers.first().compound.name,
+            self.first["Tracer Compound"],
         )
         self.assertEqual(self.animal.treatment, self.animal_treatment)
 
