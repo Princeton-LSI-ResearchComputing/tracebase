9b247ffcc1eec04d1af49636e47d205044ea5974
fkang-pu <fkang@princeton.edu>
2022-10-04 14:20:01 -0400

    added htmlSorter to column values with hyperinks for infusate and sample list

---
+++ static/js/htmlSorter.js
@@ -0,0 +1,9 @@
+// sort column based on HTML values
+// ref: https://github.com/wenzhixin/bootstrap-table/issues/461
+  function htmlSorter(a, b) {
+    var a = $(a).text();
+    var b = $(b).text();
+    if(a < b) return -1;
+    if(a > b) return 1;
+    return 0;
+}
