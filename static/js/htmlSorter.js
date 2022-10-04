45efc1cdb2e822200659700118cefd2ebb1fe950
fkang-pu <fkang@princeton.edu>
2022-10-04 15:38:00 -0400

    disbale eslint for htmlSorter.js

--- static/js/htmlSorter.js
+++ static/js/htmlSorter.js
@@ -1,9 +1,12 @@
 // sort column based on HTML values
 // ref: https://github.com/wenzhixin/bootstrap-table/issues/461
+// disable eslint in order to make sorting work with Bootstrap-Table, can't replace "var" with "let"
+/* eslint-disable */
 function htmlSorter(a, b) {
-    var a = $(a).text();
-    var b = $(b).text();
-    if(a < b) return -1;
-    if(a > b) return 1;
-    return 0;
+  var a = $(a).text();
+  var b = $(b).text();
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
 }
+/* eslint-enable */
