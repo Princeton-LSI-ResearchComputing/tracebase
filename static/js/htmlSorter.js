e21e9bc989eae14cf8dc9ef767d137f007082527
fkang-pu <fkang@princeton.edu>
2022-10-04 14:48:56 -0400

    fixed linter errors in htmlSorter.js

--- static/js/htmlSorter.js
+++ static/js/htmlSorter.js
@@ -1,6 +1,6 @@
 // sort column based on HTML values
 // ref: https://github.com/wenzhixin/bootstrap-table/issues/461
-  function htmlSorter(a, b) {
+function htmlSorter(a, b) {
     var a = $(a).text();
     var b = $(b).text();
     if(a < b) return -1;
