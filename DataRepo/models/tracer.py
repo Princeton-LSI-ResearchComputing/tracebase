bda8d07ff43bc839136900de3dd2a756660cfe08
hepcat72 <hepcat72@gmail.com>
2022-11-08 11:34:31 -0500

    isort mistake.
    Files checked in: DataRepo/models/tracer.py

--- DataRepo/models/tracer.py
+++ DataRepo/models/tracer.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from typing import Optional
 
-from django.core.exceptions import ValidationError
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from DataRepo.models.element_label import ElementLabel
