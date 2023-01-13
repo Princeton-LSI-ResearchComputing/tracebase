4e2fe1cae5271de8b1c5f79ad51892e0c2b026ff
hepcat72 <hepcat72@gmail.com>
2023-01-13 17:47:24 -0500

    Cleanup after rebase.  The code is exactly the same.  Only the history is affected.
    Files checked in: DataRepo/tests/test_views.py

--- DataRepo/tests/test_views.py
+++ DataRepo/tests/test_views.py
@@ -357,12 +357,15 @@ class ViewTests(TracebaseTestCase):
             "form-0-fld": "msrun__sample__tissue__name",
             "form-0-ncmp": "iexact",
             "form-0-val": "Brain",
+            "form-0-units": "identity",
             "form-1-pos": "pdtemplate-PeakData.0-all-False.0",
             "form-1-fld": "labels__element",
             "form-1-ncmp": "iexact",
+            "form-1-units": "identity",
             "form-2-pos": "fctemplate-FCirc.0-all-False.0",
             "form-2-fld": "msrun__sample__animal__name",
             "form-2-ncmp": "iexact",
+            "form-2-units": "identity",
         }
         qry = self.get_advanced_qry()
         dlform = {
@@ -393,6 +396,7 @@ class ViewTests(TracebaseTestCase):
                                 "ncmp": "iexact",
                                 "static": "",
                                 "val": "Brain",
+                                "units": "identity",
                             }
                         ],
                     },
@@ -412,6 +416,7 @@ class ViewTests(TracebaseTestCase):
                                 "static": "",
                                 "fld": "labels__element",
                                 "val": "",
+                                "units": "identity",
                             }
                         ],
                     },
@@ -431,6 +436,7 @@ class ViewTests(TracebaseTestCase):
                                 "ncmp": "iexact",
                                 "static": "",
                                 "val": "",
+                                "units": "identity",
                             }
                         ],
                     },
