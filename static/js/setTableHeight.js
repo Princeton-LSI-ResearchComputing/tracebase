3ccbf59617801740e7da0a34c2f302a6a20a8dfe
fkang-pu <fkang@princeton.edu>
2023-01-30 10:16:55 -0500

    more changes related to settings for table height and column width in templates

--- static/js/setTableHeight.js
+++ static/js/setTableHeight.js
@@ -3,10 +3,11 @@ set table height dynamically by comparing two values:
   input value for percentage of viewport height
   height of the table (div height)
 */
-function setTableHeight (divID, pctVH) {
-  let viewportHeight = window.innerHeight
+function setTableHeight (divID, pctVH) { // eslint-disable-line no-unused-vars
+  const viewportHeight = window.innerHeight
+  // height of the DIV section for the table
+  const divHeight = document.getElementById(divID).clientHeight
   let targetHeight
-  let divHeight
   let tabHeight
 
   // calculated height based on input value for percentage of viewport height
@@ -17,12 +18,9 @@ function setTableHeight (divID, pctVH) {
     targetHeight = 0.75 * viewportHeight
   }
 
-  // height of the DIV section for the table
-  divHeight = document.getElementById(divID).clientHeight
-
   // get table height value required by Boostrap-table for fixed header feature
   if (divHeight < targetHeight) {
-    tabHeight = divHeight
+    tabHeight = divHeight + 121
   } else {
     tabHeight = targetHeight
   }
