1329a6356bd813078a9b66f7da82ac5106270278
hepcat72 <hepcat72@gmail.com>
2023-02-05 14:18:43 -0500

    Fixed a test.
    Files checked in: DataRepo/tests/test_models.py

--- DataRepo/tests/test_models.py
+++ DataRepo/tests/test_models.py
@@ -1909,7 +1909,7 @@ class AnimalAndSampleLoadingTests(TracebaseTestCase):
             {"Sample Name": "q2", "Study Name": "TCA Flux"},
         ]
         dupes, rows = stl.get_column_dupes(data, col_keys)
-        self.assertEqual("{'Sample Name:[q2], Study Name:[TCA Flux]': [0, 1]}", dupes)
+        self.assertEqual({"Sample Name:[q2], Study Name:[TCA Flux]": [0, 1]}, dupes)
         self.assertEqual([0, 1], rows)
 
 
