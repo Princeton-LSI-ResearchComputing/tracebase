b74128b19249c152547163c474c715580501f2f0
hepcat72 <hepcat72@gmail.com>
2023-02-05 10:58:00 -0500

    Fixed limting issue.
    Files checked in: DataRepo/utils/exceptions.py

--- DataRepo/utils/exceptions.py
+++ DataRepo/utils/exceptions.py
@@ -304,11 +304,11 @@ class AggregatedErrors(Exception):
         buffered_tb_str = self.get_buffered_traceback_string()
         buffered_exception = None
         if hasattr(exception, "__traceback__") and exception.__traceback__:
-            buffered_exception = exception
             added_exc_str = "".join(
-                traceback.format_exception(type(exc), exc, exc.__traceback__)
+                traceback.format_exception(type(exception), exception, exception.__traceback__)
             )
             buffered_tb_str += f"\n\nThe above caught exception had a partial traceback:\n{added_exc_str}"
+            buffered_exception = exception
         else:
             try:
                 raise exception
