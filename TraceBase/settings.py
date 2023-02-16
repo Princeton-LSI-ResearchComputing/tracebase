07abf5badf09d52cbd6ac4e660ee73b34759291e
hepcat72 <hepcat72@gmail.com>
2023-02-16 16:44:04 -0500

    Attempt to fix remote tests that pass locally.
    Files checked in: TraceBase/settings.py

--- TraceBase/settings.py
+++ TraceBase/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
 }
 
 # This enables the validation view
-VALIDATION_ENABLED = env.bool("VALIDATION_ENABLED")
+VALIDATION_ENABLED = env.bool("VALIDATION_ENABLED", default=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
