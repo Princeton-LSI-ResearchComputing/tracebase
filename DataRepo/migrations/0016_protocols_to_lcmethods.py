50e5c578694d140349e0e640b081beb0e4dfd4cf
hepcat72 <hepcat72@gmail.com>
2023-09-20 16:43:55 -0400

    Handled the case where there is nothing to migrate.
    Files checked in: DataRepo/migrations/0016_protocols_to_lcmethods.py

--- DataRepo/migrations/0016_protocols_to_lcmethods.py
+++ DataRepo/migrations/0016_protocols_to_lcmethods.py
@@ -3,21 +3,11 @@ import re
 from django.db import migrations
 
 # Matches 25mins, 25min, 25minutes, 25minute, 25-mins, ...
-RUNLEN_PAT = re.compile(
-    r"(?P<mins_digits>\d+)[^0-9a-zA-Z]?(?:min|minute)s?"
-)
-HILIC_PAT = re.compile(
-    r"(?i)(?:hilic|lc-ms|^default$)"
-)
-REVPHASE_PAT = re.compile(
-    r"(?i)reverse"
-)
-RP_LIPID_PAT = re.compile(
-    r"(?i)lipid"
-)
-RP_IONPAIR_PAT = re.compile(
-    r"(?i)\bion\b"
-)
+RUNLEN_PAT = re.compile(r"(?P<mins_digits>\d+)[^0-9a-zA-Z]?(?:min|minute)s?")
+HILIC_PAT = re.compile(r"(?i)(?:hilic|lc-ms|^default$)")
+REVPHASE_PAT = re.compile(r"(?i)reverse")
+RP_LIPID_PAT = re.compile(r"(?i)lipid")
+RP_IONPAIR_PAT = re.compile(r"(?i)\bion\b")
 
 
 def msrunprotocol_name_to_lcmethod_rec(protocol_name, method_map, default_rec):
@@ -66,48 +56,60 @@ def msrunprotocol_to_lcmethod(apps, _):
     MSRun = apps.get_model("DataRepo", "MSRun")
     LCMethod = apps.get_model("DataRepo", "LCMethod")
 
-    method_map = [
-        {
-            # polar-HILIC-25-min
-            "MATCHING_PATS": [HILIC_PAT],  # Must match all of these
-            "UNMATCHING_PATS": [RP_LIPID_PAT, RP_IONPAIR_PAT, REVPHASE_PAT],  # Must not match any of these
-            "MINS_IF_PRESENT": 25,
-            "LC_REC": LCMethod.objects.get(name__exact="polar-HILIC-25-min")
-        },
-        {
-            # polar-reversed-phase-25-min
-            "MATCHING_PATS": [REVPHASE_PAT],  # Must match all of these
-            "UNMATCHING_PATS": [RP_LIPID_PAT, RP_IONPAIR_PAT, HILIC_PAT],  # Must not match any of these
-            "MINS_IF_PRESENT": 25,
-            "LC_REC": LCMethod.objects.get(name__exact="polar-reversed-phase-25-min")
-        },
-        {
-            # lipid-reversed-phase-25-min
-            "MATCHING_PATS": [REVPHASE_PAT, RP_LIPID_PAT],  # Must match all of these
-            "UNMATCHING_PATS": [RP_IONPAIR_PAT, HILIC_PAT],  # Must not match any of these
-            "MINS_IF_PRESENT": 25,
-            "LC_REC": LCMethod.objects.get(name__exact="lipid-reversed-phase-25-min")
-        },
-        {
-            # polar-reversed-phase-ion-pairing-25-min
-            "MATCHING_PATS": [REVPHASE_PAT, RP_IONPAIR_PAT],  # Must match all of these
-            "UNMATCHING_PATS": [RP_LIPID_PAT, HILIC_PAT],  # Must not match any of these
-            "MINS_IF_PRESENT": 25,
-            "LC_REC": LCMethod.objects.get(name__exact="polar-reversed-phase-ion-pairing-25-min")
-        },
-    ]
-    # When none of the above match:
-    default_lc_method = LCMethod.objects.get(name__exact="unknown")
+    msr_cnt = MSRun.objects.count()
+    lcm_cnt = LCMethod.objects.count()
 
-    for msrun_rec in MSRun.objects.all():
+    if msr_cnt > 0 and lcm_cnt == 0:
+        raise MissingLCMethodFixtures(msr_cnt)
+    elif msr_cnt > 0 and lcm_cnt > 0:
+        method_map = [
+            {
+                # polar-HILIC-25-min
+                "MATCHING_PATS": [HILIC_PAT],
+                "UNMATCHING_PATS": [RP_LIPID_PAT, RP_IONPAIR_PAT, REVPHASE_PAT],
+                "MINS_IF_PRESENT": 25,
+                "LC_REC": LCMethod.objects.get(name__exact="polar-HILIC-25-min"),
+            },
+            {
+                # polar-reversed-phase-25-min
+                "MATCHING_PATS": [REVPHASE_PAT],
+                "UNMATCHING_PATS": [RP_LIPID_PAT, RP_IONPAIR_PAT, HILIC_PAT],
+                "MINS_IF_PRESENT": 25,
+                "LC_REC": LCMethod.objects.get(
+                    name__exact="polar-reversed-phase-25-min"
+                ),
+            },
+            {
+                # lipid-reversed-phase-25-min
+                "MATCHING_PATS": [REVPHASE_PAT, RP_LIPID_PAT],
+                "UNMATCHING_PATS": [RP_IONPAIR_PAT, HILIC_PAT],
+                "MINS_IF_PRESENT": 25,
+                "LC_REC": LCMethod.objects.get(
+                    name__exact="lipid-reversed-phase-25-min"
+                ),
+            },
+            {
+                # polar-reversed-phase-ion-pairing-25-min
+                "MATCHING_PATS": [REVPHASE_PAT, RP_IONPAIR_PAT],
+                "UNMATCHING_PATS": [RP_LIPID_PAT, HILIC_PAT],
+                "MINS_IF_PRESENT": 25,
+                "LC_REC": LCMethod.objects.get(
+                    name__exact="polar-reversed-phase-ion-pairing-25-min"
+                ),
+            },
+        ]
+        # When none of the above match:
+        default_lc_method = LCMethod.objects.get(name__exact="unknown")
 
-        protocol_name = str(msrun_rec.protocol)
-        lcm_rec = msrunprotocol_name_to_lcmethod_rec(
-            protocol_name, method_map, default_lc_method
-        )
+        for msrun_rec in MSRun.objects.all():
+            protocol_name = str(msrun_rec.protocol)
+            lcm_rec = msrunprotocol_name_to_lcmethod_rec(
+                protocol_name, method_map, default_lc_method
+            )
 
-        msrun_rec.lc_method = lcm_rec
-        msrun_rec.save()
+            msrun_rec.lc_method = lcm_rec
+            msrun_rec.save()
+    # Else - nothing to migrate
 
 
 class Migration(migrations.Migration):
@@ -118,3 +120,12 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunPython(msrunprotocol_to_lcmethod),
     ]
+
+
+class MissingLCMethodFixtures(Exception):
+    def __init__(self, msr_cnt):
+        message = (
+            f"There are {msr_cnt} MSRun records with links to Protocol records, but 0 LCMethod records.  LCMethod "
+            "records should be loaded using DataRepo/fixtures/lc_methods.yaml before migrations can work."
+        )
+        super().__init__(message)
