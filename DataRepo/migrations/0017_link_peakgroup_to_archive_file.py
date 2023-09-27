462300e939c5506139d11071bcfafd33b605f255
Lance Parsons <lparsons@princeton.edu>
2023-09-27 15:37:51 -0400

    Reorder migrations

--- DataRepo/migrations/0017_link_peakgroup_to_archive_file.py
+++ DataRepo/migrations/0017_link_peakgroup_to_archive_file.py
@@ -18,7 +18,7 @@ def archive_file_from_filename(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("DataRepo", "0013_peakgroupset_to_archivefile"),
+        ("DataRepo", "0016_protocols_to_lcmethods"),
     ]
 
     operations = [
