851cc1951270f7fc759f94439bc89566131a58b2
hepcat72 <hepcat72@gmail.com>
2023-10-24 17:05:16 -0400

    Polished up the changes to runner.py after confirming it significantly sped up the tests.
    Files checked in: TraceBase/runner.py

--- TraceBase/runner.py
+++ TraceBase/runner.py
@@ -1,34 +1,26 @@
-import shutil
-import tempfile
-
 from django.conf import settings
 from django.test.runner import DiscoverRunner
 
 
 class TempMediaMixin(object):
-    """Mixin to create MEDIA_ROOT in temp and tear down when complete.
-
+    """
+    Mixin to use InMemoryStorage for ArchiveFile tests so that files stored in MEDIA_ROOT will not hit disk.
+    Based originally on this article from 2013, but updated to use InMemoryStorage, which is new as of Django 4.2:
     https://www.caktusgroup.com/blog/2013/06/26/media-root-and-django-tests/
     """
 
     def setup_test_environment(self):
-        "Create temp directory and update MEDIA_ROOT and default storage."
+        """Create temp directory and update MEDIA_ROOT and default storage."""
         super(TempMediaMixin, self).setup_test_environment()
-        # settings._original_media_root = settings.MEDIA_ROOT
         settings._original_file_storage = settings.DEFAULT_FILE_STORAGE
-        # self._temp_media = tempfile.mkdtemp()
-        # settings.MEDIA_ROOT = self._temp_media
         settings.DEFAULT_FILE_STORAGE = "django.core.files.storage.InMemoryStorage"
 
     def teardown_test_environment(self):
-        "Delete temp storage."
+        """Delete temp storage."""
         super(TempMediaMixin, self).teardown_test_environment()
-        # shutil.rmtree(self._temp_media, ignore_errors=True)
-        # settings.MEDIA_ROOT = settings._original_media_root
-        # del settings._original_media_root
         settings.DEFAULT_FILE_STORAGE = settings._original_file_storage
         del settings._original_file_storage
 
 
 class TraceBaseTestSuiteRunner(TempMediaMixin, DiscoverRunner):
-    "Local test suite runner."
+    """Local test suite runner."""
