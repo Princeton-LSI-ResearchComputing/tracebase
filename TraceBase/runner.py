80823b77e5fcafbfb2b7797e9a5fe3e0767a45e3
hepcat72 <hepcat72@gmail.com>
2023-10-25 13:58:54 -0400

    Tried passing args and running all tests again (because small groups of tests show no slowness)
    Files checked in: .github/workflows/tracebase-tests.yml TraceBase/runner.py

--- TraceBase/runner.py
+++ TraceBase/runner.py
@@ -4,14 +4,14 @@ from django.test.runner import DiscoverRunner
 
 class TraceBaseTestSuiteRunner(DiscoverRunner):
     """Local test suite runner."""
-    def setup_test_environment(self):
+    def setup_test_environment(self, *args, **kwargs):
         """Switch to in-memory storage."""
-        super().setup_test_environment()
+        super().setup_test_environment(*args, **kwargs)
         settings._original_file_storage = settings.DEFAULT_FILE_STORAGE
         settings.DEFAULT_FILE_STORAGE = "django.core.files.storage.InMemoryStorage"
 
-    def teardown_test_environment(self):
+    def teardown_test_environment(self, *args, **kwargs):
         """Switch back to the original file storage."""
-        super().teardown_test_environment()
+        super().teardown_test_environment(*args, **kwargs)
         settings.DEFAULT_FILE_STORAGE = settings._original_file_storage
         del settings._original_file_storage
