b92d79fa197c8d0bacce6886773ab788403455d4
hepcat72 <hepcat72@gmail.com>
2023-12-04 16:31:48 -0500

    Made custom context variables for the msrun templates.
    Files checked in: DataRepo/templates/DataRepo/msrunsample_detail.html DataRepo/templates/DataRepo/msrunsample_list.html DataRepo/templates/DataRepo/msrunsequence_detail.html DataRepo/templates/DataRepo/msrunsequence_list.html DataRepo/views/models/msrun_sample.py DataRepo/views/models/msrun_sequence.py

--- DataRepo/views/models/msrun_sample.py
+++ DataRepo/views/models/msrun_sample.py
@@ -6,9 +6,11 @@ from DataRepo.models import MSRunSample
 class MSRunSampleDetailView(DetailView):
     model = MSRunSample
     template_name = "DataRepo/msrunsample_detail.html"
+    context_object_name = "msrun_sample"
 
 
 class MSRunSampleListView(ListView):
     model = MSRunSample
     template_name = "DataRepo/msrunsample_list.html"
+    context_object_name = "msrun_samples"
     paginate_by = 20
--- DataRepo/views/models/msrun_sequence.py
+++ DataRepo/views/models/msrun_sequence.py
@@ -6,9 +6,11 @@ from DataRepo.models import MSRunSequence
 class MSRunSequenceDetailView(DetailView):
     model = MSRunSequence
     template_name = "DataRepo/msrunsequene_detail.html"
+    context_object_name = "sequences"
 
 
 class MSRunSequenceListView(ListView):
     model = MSRunSequence
     template_name = "DataRepo/msrunsequene_list.html"
+    context_object_name = "sequence"
     paginate_by = 20
