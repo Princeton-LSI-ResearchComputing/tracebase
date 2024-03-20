50279698a72484a8299200fa6e9237ca6776588a
hepcat72 <hepcat72@gmail.com>
2024-03-19 20:31:32 -0400

    Added a method to create the sample and header data in a dict.  And added tests for the methods added.
    Details:
    
    - Added get_approx_sample_header_replacement_regex which returns a regex to heuristically convert common sample headers into database sample names.
    - Added build_lcms_dict to turn sample headers from a peak annotation file and mzXML file names into an LCMS dict keyed on the unique header name and containing dicts whose keys are headers of the LCMS metadata format.
    - Modified form_valid to call build_lcms_dict with the appropriate arguments.
    - Created a class attribute to hold common header suffixes.
    - Modified set_files to build the file names if not supplied.
    - Added tests for the new methods.
    
    Files checked in: DataRepo/loaders/accucor_data_loader.py DataRepo/views/upload/validation.py DataRepo/tests/views/upload/__init__.py DataRepo/tests/views/upload/test_validation.py

---
+++ DataRepo/tests/views/upload/test_validation.py
@@ -0,0 +1,79 @@
+import re
+
+from DataRepo.tests.tracebase_test_case import TracebaseTransactionTestCase
+from DataRepo.views.upload.validation import DataValidationView
+
+
+class DataValidationViewTests(TracebaseTransactionTestCase):
+    def test_build_lcms_dict(self):
+        lcms_dict = DataValidationView.build_lcms_dict(
+            ["a", "b", "c", "c", "d_pos"],  # Sample headers
+            ["b.typo", "b.dupe", "c.mzXML", "extra.mzxml"],  # List of mzxml file names
+            "accucor.xlsx",  # Peak annot file
+        )
+        self.assertDictEqual(
+            {
+                "a": {
+                    "tracebase sample name": "a",
+                    "sample data header": "a",
+                    "peak annotation filename": "accucor.xlsx",
+                    "mzxml filename": None,
+                },
+                "b": {
+                    "tracebase sample name": "b",
+                    "sample data header": "b",
+                    "peak annotation filename": "accucor.xlsx",
+                    "mzxml filename": "b.typo",
+                },
+                "c": {
+                    "tracebase sample name": "c",
+                    "sample data header": "c",
+                    "peak annotation filename": "accucor.xlsx",
+                    "mzxml filename": "c.mzXML",
+                },
+                "d_pos": {
+                    "tracebase sample name": "d",
+                    "sample data header": "d_pos",
+                    "peak annotation filename": "accucor.xlsx",
+                    "mzxml filename": None,
+                },
+                "extra": {
+                    "tracebase sample name": "extra",
+                    "sample data header": None,
+                    "peak annotation filename": None,
+                    "mzxml filename": "extra.mzxml",
+                },
+                "ERROR: c DUPLICATE HEADER 1": {
+                    "tracebase sample name": None,
+                    "sample data header": "ERROR: c DUPLICATE HEADER 1",
+                    "peak annotation filename": "accucor.xlsx",
+                    "mzxml filename": None,
+                },
+                "ERROR: b.dupe DUPLICATE MZXML BASENAME 1": {
+                    "tracebase sample name": None,
+                    "sample data header": None,
+                    "peak annotation filename": None,
+                    "mzxml filename": "ERROR: b.dupe DUPLICATE MZXML BASENAME 1",
+                },
+            },
+            dict(lcms_dict),
+        )
+
+    def test_get_approx_sample_header_replacement_regex_default(self):
+        pattern = DataValidationView.get_approx_sample_header_replacement_regex()
+        samplename = re.sub(pattern, "", "mysample_neg_pos_scan2")
+        self.assertEqual("mysample", samplename)
+
+    def test_get_approx_sample_header_replacement_regex_add_custom(self):
+        pattern = DataValidationView.get_approx_sample_header_replacement_regex(
+            [r"_blah"]
+        )
+        samplename = re.sub(pattern, "", "mysample_pos_blah_scan1")
+        self.assertEqual("mysample", samplename)
+
+    def test_get_approx_sample_header_replacement_regex_just_custom(self):
+        pattern = DataValidationView.get_approx_sample_header_replacement_regex(
+            [r"_blah"], add=False
+        )
+        samplename = re.sub(pattern, "", "mysample_pos_blah")
+        self.assertEqual("mysample_pos", samplename)
