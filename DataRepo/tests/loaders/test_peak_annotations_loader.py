18446f25bc7c48fb1c5bbdccd17bbce2a88d23f4
hepcat72 <hepcat72@gmail.com>
2024-05-16 12:18:30 -0400

    I figured out that to have class attributes with optional values, I had to make the methods that use them into instance methods, because the @property decorator actually requires derived classes to have *instance* attributes.  The class attributes just happen to satisfy them.
    There's no actual way to force a derived class to have class attributes.  I.e. there's no way to create abstract class attributes.  So the solution was to make the classmethods into instance methods.
    
    I also added a temporary stand-in for the TableLoader class in order to test the PeakAnnotationsLoader constructor, and noted that I could not call the super constructor vias super().__init__, because ABC's constructor requires a positional argument (metaclass).
    
    Files checked in: DataRepo/loaders/peak_annotations_loader.py DataRepo/tests/loaders/test_peak_annotations_loader.py

--- DataRepo/tests/loaders/test_peak_annotations_loader.py
+++ DataRepo/tests/loaders/test_peak_annotations_loader.py
@@ -58,7 +58,7 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
         tmpdf = dict(
             (sheet, adf.copy(deep=True)) for sheet, adf in self.ACCUCOR_DF_DICT.items()
         )
-        AccucorLoader.add_df_columns(tmpdf)
+        AccucorLoader().add_df_columns(tmpdf)
         expected_df_dict = self.get_accucor_df_with_added_columns()
 
         # Sort the columns (because the column order doesn't matter)
@@ -74,12 +74,14 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
         tmpdf = dict(
             (sheet, adf.copy(deep=True)) for sheet, adf in self.ISOCORR_DF_DICT.items()
         )
-        IsocorrLoader.add_df_columns(tmpdf)
+        IsocorrLoader().add_df_columns(tmpdf)
         pd.testing.assert_frame_equal(self.ISOCORR_DF_DICT["absolte"], tmpdf["absolte"])
         self.assertEqual(len(self.ISOCORR_DF_DICT.keys()), len(tmpdf.keys()))
 
     def test_merge_df_sheets_accucor(self):
-        outdf = AccucorLoader.merge_df_sheets(self.get_accucor_df_with_added_columns())
+        outdf = AccucorLoader().merge_df_sheets(
+            self.get_accucor_df_with_added_columns()
+        )
         expected = pd.DataFrame.from_dict(
             {
                 "medMz": [104.035217, 105.038544, 74.024673, 75.028],
@@ -110,7 +112,7 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
         tmpdf = dict(
             (sheet, adf.copy(deep=True)) for sheet, adf in self.ISOCORR_DF_DICT.items()
         )
-        outdf = IsocorrLoader.merge_df_sheets(tmpdf)
+        outdf = IsocorrLoader().merge_df_sheets(tmpdf)
         # We should get back the dataframe of the absolte sheet, unchanged
         pd.testing.assert_frame_equal(self.ISOCORR_DF_DICT["absolte"], outdf)
 
@@ -138,7 +140,7 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
         tmpdf = dict(
             (sheet, adf.copy(deep=True)) for sheet, adf in self.ACCUCOR_DF_DICT.items()
         )
-        outdf = AccucorLoader.convert_df(tmpdf)
+        outdf = AccucorLoader().convert_df(tmpdf)
         expected = self.get_converted_accucor_df()
 
         # Sort the columns of both dataframes (because the column order doesn't matter)
@@ -148,7 +150,7 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
 
     def test_convert_df_accucor_tsv(self):
         with self.assertRaises(RequiredHeadersError) as ar:
-            AccucorLoader.convert_df(self.ACCUCOR_DF_DICT["Corrected"])
+            AccucorLoader().convert_df(self.ACCUCOR_DF_DICT["Corrected"])
         self.assertIn(
             "missing: ['MedMz', 'MedRt', 'IsotopeLabel', 'Formula']", str(ar.exception)
         )
@@ -177,14 +179,14 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
             (sheet, adf.copy(deep=True)) for sheet, adf in self.ISOCORR_DF_DICT.items()
         )
         expected = self.get_converted_isocorr_df()
-        outdf = IsocorrLoader.convert_df(tmpdf)
+        outdf = IsocorrLoader().convert_df(tmpdf)
         outdf = outdf.reindex(sorted(outdf.columns), axis=1)
         pd.testing.assert_frame_equal(expected, outdf)
 
     def test_convert_df_isocorr_tsv(self):
         tmpdf = self.ISOCORR_DF_DICT["absolte"].copy(deep=True)
         expected = self.get_converted_isocorr_df()
-        outdf = IsocorrLoader.convert_df(tmpdf)
+        outdf = IsocorrLoader().convert_df(tmpdf)
         outdf = outdf.reindex(sorted(outdf.columns), axis=1)
         pd.testing.assert_frame_equal(expected, outdf)
 
@@ -198,7 +200,7 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
             "Corrected": pd.DataFrame.from_dict(tmpcorr),
         }
         expected = self.get_converted_accucor_df()
-        outdf = AccucorLoader.convert_df(tmpdf)
+        outdf = AccucorLoader().convert_df(tmpdf)
         outdf = outdf.reindex(sorted(outdf.columns), axis=1)
         pd.testing.assert_frame_equal(expected, outdf)
 
@@ -211,7 +213,7 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
         tmpdict = dict(**tmporig, **tmpcorr)
         tmpdf = pd.DataFrame.from_dict(tmpdict)
         expected = self.get_converted_accucor_df()
-        outdf = AccucorLoader.convert_df(tmpdf)
+        outdf = AccucorLoader().convert_df(tmpdf)
         outdf = outdf.reindex(sorted(outdf.columns), axis=1)
         pd.testing.assert_frame_equal(expected, outdf)
 
@@ -222,7 +224,7 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
             "absolte": pd.DataFrame.from_dict(tmpabso),
         }
         expected = self.get_converted_isocorr_df()
-        outdf = IsocorrLoader.convert_df(tmpdf)
+        outdf = IsocorrLoader().convert_df(tmpdf)
         outdf = outdf.reindex(sorted(outdf.columns), axis=1)
         pd.testing.assert_frame_equal(expected, outdf)
 
@@ -231,6 +233,26 @@ class PeakAnnotationsLoaderTests(TracebaseTestCase):
         tmpabso["metaGroupId"] = [2, 2, 3, 3]
         tmpdf = pd.DataFrame.from_dict(tmpabso)
         expected = self.get_converted_isocorr_df()
-        outdf = IsocorrLoader.convert_df(tmpdf)
+        outdf = IsocorrLoader().convert_df(tmpdf)
         outdf = outdf.reindex(sorted(outdf.columns), axis=1)
+        pd.testing.assert_frame_equal(expected, outdf)
+
+    def test_AccucorLoader_constructor_conversion(self):
+        al = AccucorLoader(df=self.ACCUCOR_DF_DICT)
+        outdf = al.df
+        expected = self.get_converted_accucor_df()
+
+        # Sort the columns of both dataframes (because the column order doesn't matter)
+        outdf = outdf.reindex(sorted(outdf.columns), axis=1)
+
+        pd.testing.assert_frame_equal(expected, outdf)
+
+    def test_IsocorrLoader_constructor_conversion(self):
+        il = IsocorrLoader(df=self.ISOCORR_DF_DICT)
+        outdf = il.df
+        expected = self.get_converted_isocorr_df()
+
+        # Sort the columns of both dataframes (because the column order doesn't matter)
+        outdf = outdf.reindex(sorted(outdf.columns), axis=1)
+
         pd.testing.assert_frame_equal(expected, outdf)
