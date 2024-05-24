b476ba55fc625918b320da7d92eac27807c96b0b
hepcat72 <hepcat72@gmail.com>
2024-05-24 10:46:25 -0400

    TableLoader derived class doc string update.
    The arguments to the parent class constructor are provided for convenience.
    
    Files checked in: DataRepo/loaders/compounds_loader.py DataRepo/loaders/msruns_loader.py DataRepo/loaders/peak_annotations_loader.py DataRepo/loaders/tracers_loader.py

--- DataRepo/loaders/peak_annotations_loader.py
+++ DataRepo/loaders/peak_annotations_loader.py
@@ -212,7 +212,7 @@ class PeakAnnotationsLoader(ConvertedTableLoader, ABC):
                     MUST HANDLE THE ROLLBACK.
                 data_sheet (Optional[str]) [None]: Sheet name (for error reporting).
                 defaults_sheet (Optional[str]) [None]: Sheet name (for error reporting).
-                *file (Optional[str]) [None]: File name (for error reporting).
+                file (Optional[str]) [None]: File name (for error reporting).
                 user_headers (Optional[dict]): Header names by header key.
                 defaults_df (Optional[pandas dataframe]): Default values data from a table-like file.
                 defaults_file (Optional[str]) [None]: Defaults file name (None if the same as infile).
@@ -220,6 +220,10 @@ class PeakAnnotationsLoader(ConvertedTableLoader, ABC):
                 defaults (Optional[DefaultsTableHeaders namedtuple]): default values by header key.
                 extra_headers (Optional[List[str]]): Use for dynamic headers (different in every file).  To allow any
                     unknown header, supply an empty list.
+                _validate (bool): If true, runs in validate mode, perhaps better described as "non-curator mode".  This
+                    is intended for use by the web validation interface.  It's similar to dry-run mode, in that it never
+                    commits anything, but it also raises warnings as fatal (so they can be reported through the web
+                    interface and seen by researchers, among other behaviors specific to non-privileged users).
             Derived (this) class Args:
                 peak_annotation_details_file (Optional[str]): The name of the file that the Peak Annotation Details came
                     from.
