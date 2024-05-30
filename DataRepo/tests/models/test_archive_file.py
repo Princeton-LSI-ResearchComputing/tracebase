f44456ee536a98995c78188bc626100bc9e90604
hepcat72 <hepcat72@gmail.com>
2024-05-30 18:01:09 -0400

    Added the ability for ArchiveFile to clean up the archive after the commit of a delete.

--- DataRepo/tests/models/test_archive_file.py
+++ DataRepo/tests/models/test_archive_file.py
@@ -1,11 +1,17 @@
+import os
 from pathlib import Path
 
 from django.conf import settings
 from django.core.files import File
+from django.db import transaction
 from django.test import override_settings
 
 from DataRepo.models import ArchiveFile, DataFormat, DataType
-from DataRepo.tests.tracebase_test_case import TracebaseTestCase
+from DataRepo.models.utilities import exists_in_db
+from DataRepo.tests.tracebase_test_case import (
+    TracebaseArchiveTestCase,
+    TracebaseTestCase,
+)
 
 
 @override_settings(CACHES=settings.TEST_CACHES)
@@ -59,6 +65,97 @@ class ArchiveFileTests(TracebaseTestCase):
         fn = "DataRepo/data/tests/small_obob/small_obob_maven_6eaas_inf_lactate_mzxmls/BAT-xz971.mzXML"
         self.assertFalse(ArchiveFile.file_is_binary(fn))
 
+
+class ArchiveFileArchiveTests(TracebaseArchiveTestCase):
+    record_id = None
+    fixtures = ["data_types.yaml", "data_formats.yaml"]
+
+    @classmethod
+    def setUpTestData(cls):
+        path = Path("DataRepo/data/tests/small_obob/small_obob_maven_6eaas_inf.xlsx")
+        with path.open(mode="rb") as f:
+            myfile = File(f, name=path.name)
+        ms_peak_annotation = DataType.objects.get(code="ms_peak_annotation")
+        accucor_format = DataFormat.objects.get(code="accucor")
+        cls.rec_dict = {
+            "filename": "small_obob_maven_6eaas_inf.xlsx",
+            "file_location": myfile,
+            "checksum": "558ea654d7f2914ca4527580edf4fac11bd151c3",
+            "data_type": ms_peak_annotation,
+            "data_format": accucor_format,
+        }
+        super().setUpTestData()
+
+    def setUp(self):
+        super().setUp()
+        self.rec = None
+
+    def create_archive_file(self):
+        path = Path("DataRepo/data/tests/small_obob/small_obob_maven_6eaas_inf.xlsx")
+        with path.open(mode="rb") as f:
+            myfile = File(f, name=path.name)
+            ms_peak_annotation = DataType.objects.get(code="ms_peak_annotation")
+            accucor_format = DataFormat.objects.get(code="accucor")
+            rec_dict = {
+                "filename": "small_obob_maven_6eaas_inf.xlsx",
+                "file_location": myfile,
+                "checksum": "558ea654d7f2914ca4527580edf4fac11bd151c3",
+                "data_type": ms_peak_annotation,
+                "data_format": accucor_format,
+            }
+            self.rec = ArchiveFile.objects.create(**rec_dict)
+            self.rec.full_clean()
+        self.assertTrue(
+            os.path.isfile(self.rec.file_location.path),
+            msg="Asserts created file was saved to disk.",
+        )
+
+    @transaction.atomic
+    def delete_during_transaction_failure(self):
+        self.create_archive_file()
+        self.rec.delete()
+        self.assertTrue(
+            os.path.isfile(self.rec.file_location.path),
+            msg="Asserts created file does not delete during transaction.",
+        )
+        raise ValueError("TEST")
+
+    @transaction.atomic
+    def delete_during_transaction_success(self):
+        self.create_archive_file()
+        self.rec.delete()
+        self.assertTrue(
+            os.path.isfile(self.rec.file_location.path),
+            msg="Asserts created file does not delete during transaction.",
+        )
+
+    def test_post_delete_commit_failure(self):
+        """Tests that archive files are cleaned up after rollback."""
+        try:
+            self.delete_during_transaction_failure()
+        except ValueError:
+            pass
+        self.assertIsNotNone(self.rec)
+        self.assertIsNotNone(self.rec.file_location)
+        self.assertIn(self.ARCHIVE_DIR, str(self.rec.file_location.path))
+        self.assertTrue(
+            os.path.isfile(self.rec.file_location.path),
+            msg="Asserts created file was not deleted due to transaction rollback.",
+        )
+        self.assertFalse(exists_in_db(self.rec))
+
+    def test_post_delete_commit_success(self):
+        """Tests that archive files are cleaned up after rollback."""
+        self.delete_during_transaction_success()
+        self.assertIsNotNone(self.rec)
+        self.assertIsNotNone(self.rec.file_location)
+        self.assertIn(self.ARCHIVE_DIR, str(self.rec.file_location.path))
+        self.assertFalse(
+            os.path.isfile(self.rec.file_location.path),
+            msg="Asserts created file was deleted after transaction commit due to call to record delete.",
+        )
+        self.assertFalse(exists_in_db(self.rec))
+
     def test_get_or_create_override(self):
         """This tests the essential functionality of the get_or_create method override, which is to ignore the
         randomized hash string appended to file_location.  but it also tests the conveniences (takes a path string,
@@ -82,10 +179,16 @@ class ArchiveFileTests(TracebaseTestCase):
         self.assertEqual("mzxml", created_rec.data_format.code)
         self.assertEqual("ms_data", created_rec.data_type.code)
         self.assertEqual("BAT-xz971.mzXML", created_rec.filename)
+        self.assertTrue(
+            os.path.isfile(created_rec.file_location.path),
+            msg="Asserts mzXML file was created in the archive.",
+        )
 
         # Called a second time to "get"
         gotten_rec, second_created = ArchiveFile.objects.get_or_create(**rec_dict)
         self.assertFalse(second_created)
         self.assertEqual(created_rec.id, gotten_rec.id)
-
-        # TODO: Figure out how to test that the file_location is an actual stored file
+        self.assertTrue(
+            os.path.isfile(gotten_rec.file_location.path),
+            msg="Asserts mzXML file still exists.",
+        )
