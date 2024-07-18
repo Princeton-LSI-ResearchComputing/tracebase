b49083f9ac7ec25c56045755c67ca10d85d04ee8
hepcat72 <hepcat72@gmail.com>
2024-07-18 18:03:42 -0400

    Added the new load_study script.
    Also add TODO comments for removing code that issues errors if old parameters are supplied.
    
    Files checked in: DataRepo/management/commands/load_samples.py DataRepo/management/commands/load_study.py

--- DataRepo/management/commands/load_study.py
+++ DataRepo/management/commands/load_study.py
@@ -4,15 +4,19 @@ from typing import Type
 from django.core.management import CommandError
 
 from DataRepo.loaders.base.table_loader import TableLoader
-from DataRepo.loaders.samples_loader import SamplesLoader
+from DataRepo.loaders.study_loader import StudyLoader
 from DataRepo.management.commands.load_table import LoadTableCommand
 
 
 class Command(LoadTableCommand):
-    """Command to load the Sample model from a table-like file."""
+    """Command to load all sheets of an entire study doc.
 
-    help = "Loads samples from a table-like file into the database"
-    loader_class: Type[TableLoader] = SamplesLoader
+    NOTE: loader_class takes a derived class of TableLoader named StudyLoader, but anywhere it refers to columns and
+    headers, in this context, it's referring to sheets and tabs.
+    """
+
+    help = "Loads all data from a study doc (e.g. Animals, Samples, Compounds, etc) into the database."
+    loader_class: Type[TableLoader] = StudyLoader
 
     # TODO: Remove this after all dependent code has been updated for the new version of this script
     def add_arguments(self, parser):
@@ -21,9 +25,9 @@ class Command(LoadTableCommand):
 
         parser.add_argument(
             # Legacy support - catch this option and issue an error if it is used.
-            "--sample-table-filename",
-            action="store_true",
-            default=False,
+            "study_params",
+            type=argparse.FileType("r"),
+            nargs="?",
             help=argparse.SUPPRESS,
         )
 
@@ -41,17 +45,17 @@ class Command(LoadTableCommand):
                 - Unique file constraints
 
         Args:
-            options (dict): Values provided on the command line.
-        Exceptions:
+            options (dict of strings): String values provided on the command line by option name.
+        Raises:
             None
         Returns:
             None
         """
         # TODO: Remove this after all dependent code has been updated for the new version of this script
-        if options["sample_table_filename"]:
+        if options["study_params"] is not None:
             raise CommandError(
-                "By supplying --sample-table-filename, it looks like you're trying to call the old version of this "
-                "script.  This script has been renamed.  Use `pythong manage.py legacy_load_samples ...` instead."
+                "By supplying a positional argument, it looks like you're trying to call the old version of this "
+                "script.  This script has been renamed.  Use `pythong manage.py legacy_load_study ...` instead."
                 f"{options}"
             )
 
