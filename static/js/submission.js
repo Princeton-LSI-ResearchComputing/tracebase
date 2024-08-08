3a48dbd931782f7d452a5f3e533892ca2334c403
hepcat72 <hepcat72@gmail.com>
2024-08-08 18:16:42 -0400

    Addressed javascript linting issues.
    Files checked in: static/js/submission.js

--- static/js/submission.js
+++ static/js/submission.js
@@ -1,44 +1,47 @@
+// TODO: Figure out how to import methods from other files: browserDownloadExcel, getFileNamesString, DataTransfer
+// See https://stackoverflow.com/questions/950087/
 document.addEventListener('DOMContentLoaded', function () {
-  peak_annot_input = document.getElementById('peak_annotation_files_field')
-  peak_annot_list = document.getElementById('pending_peak_annot_files')
-  study_file_content_tag = document.getElementById('output_study_file')
-  current_peak_annot_files = null
+  const peakAnnotInput = document.getElementById('peak_annotation_files_field')
+  const peakAnnotList = document.getElementById('pending_peak_annot_files')
+  const studyFileContentTag = document.getElementById('output_study_file')
+  let currentPeakAnnotFiles = null
 
   // If there is a study file that was produced
-  if (typeof study_file_content_tag !== 'undefined' && study_file_content_tag) {
-    browserDownloadExcel('{{ study_filename }}', study_file_content_tag.innerHTML)
+  if (typeof studyFileContentTag !== 'undefined' && studyFileContentTag) {
+    browserDownloadExcel('{{ study_filename }}', studyFileContentTag.innerHTML) // eslint-disable-line no-undef
   }
 
   // Note that the reset button does not trigger a change event, so see its onclick code
-  peak_annot_input.addEventListener(
+  peakAnnotInput.addEventListener(
     'change',
-    function () { handlePeakAnnotFiles(peak_annot_input.files) },
+    function () { handlePeakAnnotFiles(peakAnnotInput.files) },
     false
   )
+
+  function showPeakAnnotFiles (files) {
+    peakAnnotList.innerHTML = getFileNamesString(files) // eslint-disable-line no-undef
+  }
 
   function handlePeakAnnotFiles (files, add) {
     if (typeof add === 'undefined' || add === null) {
       add = true
     }
     // If we're adding files, oreviously selected files exist, and the field isn't being cleared
-    if (add && typeof current_peak_annot_files !== 'undefined' && current_peak_annot_files && files.length > 0) {
-      const dT = new DataTransfer()
+    if (add && typeof currentPeakAnnotFiles !== 'undefined' && currentPeakAnnotFiles && files.length > 0) {
+      // DataTransfer comes from browsers
+      const dT = new DataTransfer() // eslint-disable-line no-undef
       // Add previously added files to the DataTranfer object
-      for (let i = 0; i < current_peak_annot_files.length; i++) {
-        dT.items.add(current_peak_annot_files[i])
+      for (let i = 0; i < currentPeakAnnotFiles.length; i++) {
+        dT.items.add(currentPeakAnnotFiles[i])
       }
       // Add newly selected files to the DataTranfer object
       for (let i = 0; i < files.length; i++) {
         dT.items.add(files[i])
       }
       // Set the file input element to the combined (previously added and new) files
-      peak_annot_input.files = dT.files
+      peakAnnotInput.files = dT.files
     }
-    current_peak_annot_files = peak_annot_input.files
-    showPeakAnnotFiles(peak_annot_input.files)
+    currentPeakAnnotFiles = peakAnnotInput.files
+    showPeakAnnotFiles(peakAnnotInput.files)
   }
 })
-
-function showPeakAnnotFiles (files) {
-  peak_annot_list.innerHTML = getFileNamesString(files)
-}
