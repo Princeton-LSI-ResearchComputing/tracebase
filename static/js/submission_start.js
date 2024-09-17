b6cb7ec0c5e3dc98418701faae1a3f668c2edf89
hepcat72 <hepcat72@gmail.com>
2024-09-17 17:29:05 -0400

    Added the ability to disable and enable the submit button.
    Added doc strings and made some minor improvements.
    
    Files checked in: DataRepo/templates/submission/includes/1_start.html DataRepo/templates/submission/submission.html static/js/submission_start.js
    [skip ci]

--- static/js/submission_start.js
+++ static/js/submission_start.js
@@ -1,6 +1,7 @@
 var templateContainer = null // eslint-disable-line no-var
 var peakAnnotFormsTable = null // eslint-disable-line no-var
 var dropAreaInput = null // eslint-disable-line no-var
+var dataSubmissionForm = null // eslint-disable-line no-var
 
 /**
  * A method to initialize the peak annotation file form interface.  Dropped files will call addPeakAnnotFileToUpload,
@@ -10,11 +11,14 @@ var dropAreaInput = null // eslint-disable-line no-var
  * @param {*} peakAnnotFormsTable [table] The table where the form rows will be added when files are dropped in the drop
  * area.
  */
-function initPeakAnnotUploads(templateContainer, peakAnnotFormsTable, dropAreaInput) {
+function initPeakAnnotUploads(templateContainer, peakAnnotFormsTable, dropAreaInput, dataSubmissionForm) {
     globalThis.templateContainer = templateContainer;
     globalThis.peakAnnotFormsTable = peakAnnotFormsTable;
     globalThis.dropAreaInput = dropAreaInput;
-  }
+    globalThis.dataSubmissionForm = dataSubmissionForm;
+    // Disable the form submission button to start (because there are no peak annotation form rows yet.
+    disablePeakAnnotForm();
+}
 
 /**
  * This method takes a single peak annotation file for upload (inside a DataTransfer object) and clones a file upload
@@ -27,6 +31,11 @@ function addPeakAnnotFileToUpload(dT, template) { // eslint-disable-line no-var
   peakAnnotFormsTable.appendChild(newRow);
 }
 
+/**
+ * This function clones a tr row containing the form elements for a peak annotation file.
+ * @param {*} template - The tr element for the template to clone.
+ * @returns the cloned row element containing the cloned form for a peak annot file.
+ */
 function createPeakAnnotFormRow(template) {
   if (typeof template === 'undefined' || !template) {
       template = templateContainer;
@@ -34,6 +43,11 @@ function createPeakAnnotFormRow(template) {
     return template.cloneNode(true);
 }
 
+/**
+ * Un-hide the file input column and set the files of the file input.
+ * @param {*} dT - DataTransfer object containing 1 file in its files attribute.
+ * @param {*} formRow - The row element containing the form.
+ */
 function makeFormModifications(dT, formRow) {
   fileTd = formRow.querySelector('#fileColumn');
   fileTd.style = null;
@@ -42,14 +56,34 @@ function makeFormModifications(dT, formRow) {
 }
 
 /**
+ * This function enables the form submission button.
+ */
+function enablePeakAnnotForm() {
+  submitInput = dataSubmissionForm.querySelector('#submit');
+  submitInput.removeAttribute('disabled');
+}
+
+/**
+ * This function disables the form submission button.
+ */
+function disablePeakAnnotForm() {
+  submitInput = dataSubmissionForm.querySelector('#submit');
+  submitInput.disabled = true;
+}
+
+/**
  * This function clears the file picker input element inside the drop area after having created form rows.  It is called
  * from the drop-area code after all dropped/picked files have been processed.  It intentionally leaves the entries in
  * the sequence metadata inputs for re-use upon additional drops/picks.
  */
 function afterAddingFiles() {
-  globalThis.dropAreaInput.value = null;
+  dropAreaInput.value = null;
+  enablePeakAnnotForm();
 }
 
+/**
+ * This function clears all of the previously added peak annotation form rows.
+ */
 function clearPeakAnnotFiles() {
   peakAnnotFormsTable.innerHTML = '';
 }
