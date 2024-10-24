77d866a757ad19c557cecfb5bcb4823dae44e9e5
hepcat72 <hepcat72@gmail.com>
2024-10-23 20:07:47 -0400

    Added more data to the mzXML metadata file (more columns), as requested by Michael, and added data to the streamed files to check in the tests.
    Files checked in: DataRepo/tests/views/search/test_download.py DataRepo/views/search/download.py

--- DataRepo/tests/views/search/test_download.py
+++ DataRepo/tests/views/search/test_download.py
@@ -66,7 +66,21 @@ def assert_StreamingHttpResponse(testcase_obj, response):
 
 
 class AdvancedSearchDownloadViewTests(TracebaseTestCase):
+    fixtures = ["lc_methods.yaml", "data_types.yaml", "data_formats.yaml"]
+
     qry = test_qry
+
+    @classmethod
+    def setUpTestData(cls):
+        sl = StudyV3Loader(
+            file="DataRepo/data/tests/full_tiny_study/study.xlsx",
+            df=read_from_file(
+                "DataRepo/data/tests/full_tiny_study/study.xlsx", sheet=None
+            ),
+            mzxml_dir="DataRepo/data/tests/full_tiny_study",
+        )
+        sl.load_data()
+        super().setUpTestData()
 
     def test_get_qry(self):
         form_data = {"qryjson": json.dumps(self.qry)}
@@ -79,7 +93,7 @@ class AdvancedSearchDownloadViewTests(TracebaseTestCase):
     def test_get_query_results(self):
         asdv = AdvancedSearchDownloadView()
         res = asdv.get_query_results(self.qry)
-        self.assertEqual(0, res.count())
+        self.assertEqual(36, res.count())
 
     def test_form_valid(self):
         form_data = {"qryjson": json.dumps(self.qry)}
@@ -107,10 +121,28 @@ class AdvancedSearchDownloadViewTests(TracebaseTestCase):
             "Feeding Status\tTreatment\tInfusate\tTracer(s)\tTracer Compound(s)\tTracer Concentration(s) (mM)\t"
             "Infusion Rate (ul/min/g)\tStudies\n"
         ).encode()
+        expected3 = (
+            "xzl5_t\tserum_plasma_tail\t150.0\talanine\talanine\talanine/L-alanine/ala\tC3H7NO2\tC\txzl5_t.mzXML\t"
+            "6587116.743783121\t0.2235244143081364\t1472381.4121334408\t1.0\talafasted_cor.xlsx\txzl5\tWT\t27.5\t14.0\t"
+            "M\tPicoLab Rodent 20 5053\tfasted\tno treatment\talanine-[13C3,15N1][180]\talanine-[13C3,15N1]\talanine\t"
+            "180.0\t0.1\ttest v3 study\nxzl5_t\tserum_plasma_tail\t150.0\talanine\talanine\talanine/L-alanine/ala\t"
+            "C3H7NO2\tN\txzl5_t.mzXML\t6587116.743783121\t0.30075567022988536\t1981112.7111589927\t1.0\t"
+            "alafasted_cor.xlsx\txzl5\tWT\t27.5\t14.0\tM\tPicoLab Rodent 20 5053\tfasted\tno treatment\t"
+            "alanine-[13C3,15N1][180]\talanine-[13C3,15N1]\talanine\t180.0\t0.1\ttest v3 study\nxzl5_panc\tpancreas\t"
+            "150.0\talanine\talanine\talanine/L-alanine/ala\tC3H7NO2\tC\txzl5_panc.mzXML\t43695995.42306948\t"
+            "0.06929539990964839\t3027931.477291765\t0.31001266740429617\talafasted_cor.xlsx\txzl5\tWT\t27.5\t14.0\tM\t"
+            "PicoLab Rodent 20 5053\tfasted\tno treatment\talanine-[13C3,15N1][180]\talanine-[13C3,15N1]\talanine\t"
+            "180.0\t0.1\ttest v3 study\nxzl5_panc\tpancreas\t150.0\talanine\talanine\talanine/L-alanine/ala\tC3H7NO2\tN"
+            "\txzl5_panc.mzXML\t43695995.42306948\t0.09250135755812483\t4041938.896487536\t0.3075631375043422\t"
+            "alafasted_cor.xlsx\txzl5\tWT\t27.5\t14.0\tM\tPicoLab Rodent 20 5053\tfasted\tno treatment\t"
+            "alanine-[13C3,15N1][180]\talanine-[13C3,15N1]\talanine\t180.0\t0.1\ttest v3 study\nxzl4_t\t"
+            "serum_plasma_tail\t150.0"  # A portion of the file
+        ).encode()
         content = str(response.getvalue())
         # `[2:-1]` removes the "b'" and last "'" from the beginning and end of the converted bytes to string
         self.assertIn(str(expected1)[2:-1], content)
         self.assertIn(str(expected2)[2:-1], content)
+        self.assertIn(str(expected3)[2:-1], content)
 
 
 class RecordToMzxmlTSVTests(TracebaseTestCase):
@@ -141,8 +173,18 @@ class RecordToMzxmlTSVTests(TracebaseTestCase):
                 "MZ Min",
                 "MZ Max",
                 "Sample",
-                "Animal",
                 "Tissue",
+                "Date Collected",
+                "Collection Time (m)",
+                "Handler",
+                "Animal",
+                "Age",
+                "Sex",
+                "Genotype",
+                "Weight (g)",
+                "Diet",
+                "Feeding Status",
+                "Treatment",
                 "Infusate",
                 "Operator",
                 "Instrument",
@@ -168,8 +210,18 @@ class RecordToMzxmlTSVTests(TracebaseTestCase):
                 1.0,
                 502.9,
                 "xzl5_t",
-                "xzl5",
                 "serum_plasma_tail",
+                "2020-07-22",
+                150.0,
+                "Xianfeng Zeng",
+                "xzl5",
+                14.0,
+                "M",
+                "WT",
+                27.5,
+                "PicoLab Rodent 20 5053",
+                "fasted",
+                "no treatment",
                 "alanine-[13C3,15N1][180]",
                 "Xianfeng Zeng",
                 "QE2",
@@ -190,8 +242,18 @@ class RecordToMzxmlTSVTests(TracebaseTestCase):
                     1.0,
                     502.9,
                     "xzl5_t",
-                    "xzl5",
                     "serum_plasma_tail",
+                    "2020-07-22",
+                    150.0,
+                    "Xianfeng Zeng",
+                    "xzl5",
+                    14.0,
+                    "M",
+                    "WT",
+                    27.5,
+                    "PicoLab Rodent 20 5053",
+                    "fasted",
+                    "no treatment",
                     "alanine-[13C3,15N1][180]",
                     "Xianfeng Zeng",
                     "QE2",
@@ -216,8 +278,18 @@ class RecordToMzxmlTSVTests(TracebaseTestCase):
                     1.0,
                     502.9,
                     "xzl5_panc",
-                    "xzl5",
                     "pancreas",
+                    "2020-07-22",
+                    150.0,
+                    "Xianfeng Zeng",
+                    "xzl5",
+                    14.0,
+                    "M",
+                    "WT",
+                    27.5,
+                    "PicoLab Rodent 20 5053",
+                    "fasted",
+                    "no treatment",
                     "alanine-[13C3,15N1][180]",
                     "Xianfeng Zeng",
                     "QE2",
@@ -230,6 +302,20 @@ class RecordToMzxmlTSVTests(TracebaseTestCase):
 
 
 class AdvancedSearchDownloadMzxmlTSVViewTests(TracebaseTestCase):
+    fixtures = ["lc_methods.yaml", "data_types.yaml", "data_formats.yaml"]
+
+    @classmethod
+    def setUpTestData(cls):
+        sl = StudyV3Loader(
+            file="DataRepo/data/tests/full_tiny_study/study.xlsx",
+            df=read_from_file(
+                "DataRepo/data/tests/full_tiny_study/study.xlsx", sheet=None
+            ),
+            mzxml_dir="DataRepo/data/tests/full_tiny_study",
+        )
+        sl.load_data()
+        super().setUpTestData()
+
     def test_form_valid(self):
         form_data = {"qryjson": json.dumps(test_qry)}
         form = AdvSearchDownloadForm(data=form_data)
@@ -240,16 +326,15 @@ class AdvancedSearchDownloadMzxmlTSVViewTests(TracebaseTestCase):
         assert_StreamingHttpResponse(self, response)
         expected1 = "# Download Time: ".encode()
         expected2 = (
-            "# Advanced Search Query: {'selectedtemplate': 'pgtemplate', 'searches': {'pgtemplate': {'name': "
-            "'PeakGroups', 'tree': {'type': 'group', 'val': 'all', 'static': False, 'queryGroup': [{'type': 'query', "
-            "'pos': '', 'static': False, 'fld': '', 'ncmp': '', 'val': '', 'units': ''}]}}, 'pdtemplate': {'name': "
-            "'PeakData', 'tree': {'type': 'group', 'val': 'all', 'static': False, 'queryGroup': [{'type': 'query', "
-            "'pos': '', 'static': False, 'fld': '', 'ncmp': '', 'val': '', 'units': ''}]}}, 'fctemplate': {'name': "
-            "'Fcirc', 'tree': {'type': 'group', 'val': 'all', 'static': False, 'queryGroup': [{'type': 'query', "
-            "'pos': '', 'static': False, 'fld': '', 'ncmp': '', 'val': '', 'units': ''}]}}}}\n"
-            "#\n"
-            "mzXML File\tPolarity\tMZ Min\tMZ Max\tSample\tAnimal\tTissue\tInfusate\tOperator\tInstrument\tLC Protocol"
-            "\tDate\r\n"
+            "nmzXML File\tPolarity\tMZ Min\tMZ Max\tSample\tTissue\tDate Collected\tCollection Time (m)\tHandler\t"
+            "Animal\tAge\tSex\tGenotype\tWeight (g)\tDiet\tFeeding Status\tTreatment\tInfusate\tOperator\tInstrument\t"
+            "LC Protocol\tDate\r\n2020-07-22/Xianfeng Zeng/QE2/polar-HILIC-25-min/positive/1-503/xzl5_t.mzXML\tpositive"
+            "\t1.0\t502.9\txzl5_t\tserum_plasma_tail\t2020-07-22\t150.0\tXianfeng Zeng\txzl5\t14.0\tM\tWT\t27.5\t"
+            "PicoLab Rodent 20 5053\tfasted\tno treatment\talanine-[13C3,15N1][180]\tXianfeng Zeng\tQE2\t"
+            "polar-HILIC-25-min\t2020-07-22\r\n2020-07-22/Xianfeng Zeng/QE2/polar-HILIC-25-min/positive/1-503/"
+            "xzl5_t.mzXML\tpositive\t1.0\t502.9\txzl5_t\tserum_plasma_tail\t2020-07-22\t150.0\tXianfeng Zeng\txzl5\t"
+            "14.0\tM\tWT\t27.5\tPicoLab Rodent 20 5053\tfasted\tno treatment\talanine-[13C3,15N1][180]\tXianfeng Zeng\t"
+            "QE2\tpolar-HILIC-25-min\t2020-07-22\r\n"  # There is more, but this is sufficient
         ).encode()
         content = str(response.getvalue())
         # `[2:-1]` removes the "b'" and last "'" from the beginning and end of the converted bytes to string
