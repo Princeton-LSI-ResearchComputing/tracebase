835d322082f3179a29c0a1305bb8a4f685bf877f
hepcat72 <hepcat72@gmail.com>
2025-02-10 12:25:56 -0500

    ListView pagination compatible with bootstrap-table search, sort, and export.
    This is still just sketched out, but I just need to trigger reloads with all of the parameters, such as offset, limit, search, order, sort, and all of the column filter values.  Then I need to add javascript to re-render those things so that what the user sees makes sense.  E.g. They see the purple arrow showing that the surrent sort is on column X and that column Y is currently filtered, they're on page Z, and when they export, all of the data is triggered to download (not just the current page worth).
    
    Files checked in: DataRepo/templates/DataRepo/archive_file_list.html DataRepo/views/models/archive_file.py
    [skip ci]

--- DataRepo/views/models/archive_file.py
+++ DataRepo/views/models/archive_file.py
@@ -1,4 +1,5 @@
-from django.db.models.functions import Coalesce
+from django.core.paginator import Paginator
+from django.db.models import Func, F, Q, Value, CharField, QuerySet
 from django.views.generic import DetailView, ListView
 
 from DataRepo.models import ArchiveFile
@@ -10,20 +11,112 @@ class ArchiveFileListView(ListView):
     model = ArchiveFile
     context_object_name = "archive_file_list"
     template_name = "DataRepo/archive_file_list.html"
+    paginate_by = 25
+    DATETIME_FORMAT = "Mon. DD, YYYY, h:MI a.m."  # TODO: postgres-specific. Disable if not postgres.
+    DBSTRING_FUNCTION = "to_char"  # TODO: postgres-specific. Disable if not postgres.
 
-    # def get_queryset(self):
-    #     fields = list(ArchiveFile._meta.__dict__["ordering"]).extend(["id", "studies"])
-    #     return (
-    #         super()
-    #         .get_queryset()
-    #         .annotate(
-    #             studies=Coalesce(
-    #                 "peak_groups__msrun_sample__sample__animal__studies",
-    #                 "mz_to_msrunsamples__sample__animal__studies",
-    #                 "raw_to_msrunsamples__sample__animal__studies",
-    #             ),
-    #         )
-    #     ).order_by(*fields).distinct(*fields)
+    # TODO: Make a base class that supports ListView combined with bootstrap-table to make pages load faster, using
+    # server-side behavior for pagination
+
+    def get_queryset(self):
+        print("GETTING QS", flush=True)
+        qs = super().get_queryset()
+        print(f"SETTING TOTAL {qs.count()}")
+        self.total = qs.count()
+        self.raw_total = self.total
+        print("GETTING PAGINATED QS", flush=True)
+        return self.get_paginated_queryset(qs)
+
+    def get_paginated_queryset(self, qs: QuerySet[ArchiveFile]):
+        # See if there is search and/or sort criteria
+        search_term = self.request.GET.get("search")
+        if search_term == "":
+            search_term = None
+        limit = self.request.GET.get("limit", "")
+        if limit == "":
+            limit = self.paginate_by
+        else:
+            limit = int(limit)
+        offset = self.request.GET.get("offset", "")
+        if offset == "":
+            offset = 0
+        else:
+            offset = int(offset)
+        sort = self.request.GET.get("sort")
+        order = self.request.GET.get("order")
+
+        print(f"search_term: {search_term} limit: {limit} offset: {offset} sort: {sort} order: {order}", flush=True)
+
+        # Perform a search if one is defined
+        if search_term:
+            # Convert the date time field into a string to search with icontains from a bootstrap table search, using
+            # the default django datetime format (i.e. what they see in the template)
+            qs.annotate(imported_timestamp_str=Func(
+                F("imported_timestamp"),
+                Value(self.DATETIME_FORMAT),
+                output_field=CharField(),
+                function=self.DBSTRING_FUNCTION)
+            )
+
+            q_exp = self.get_any_field_query(search_term)
+
+            qs = qs.filter(q_exp)
+
+        # Set the total after the search
+        print(f"SETTING QUERY TOTAL {qs.count()}")
+        self.total = qs.count()
+
+        # Sort the results, if sort has a value
+        if sort:
+            if order is not None and order.lower().startswith("d"):
+                sort = f"-{sort}"
+            qs = qs.order_by(sort)
+
+        # Limit the number of results returned using the offset and limit
+        qs = qs[offset:limit]
+
+        print(f"RETURNING QS WITH {qs.count()} RECORDS", flush=True)
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        """This sets up djang-compatible pagination, search, and sort"""
+
+        print("GETTING CONTEXT", flush=True)
+
+        context = super().get_context_data(**kwargs)
+
+        print("CREATING PAGINATOR", flush=True)
+
+        # Get pagination info
+        paginator = Paginator(context[self.context_object_name], self.paginate_by)
+
+        print("GETTING PAGE", flush=True)
+
+        page = self.request.GET.get("page")
+        page_obj = paginator.get_page(page)
+        context["page_obj"] = page_obj
+
+        print("SETTING PAGE OBJ LIST", flush=True)
+
+        # Ensure archive_file_list is paginated
+        context[self.context_object_name] = page_obj.object_list
+
+        print(f"RETURNING CONTEXT {context}\nPAGINATOR: {[(k, getattr(context['paginator'], k)) for k in dir(context['paginator'])]}", flush=True)
+
+        return context
+
+    def get_any_field_query(self, term: str):
+        """Given a string search term, returns a Q expression that does a case-insensitive search of all fields from the
+        table displayed in the template.  Note, annotation fields must be generated in order to apply the query.
+        E.g. `imported_timestamp` must be converted to an annotation field named `imported_timestamp_str`."""
+
+        print("GETTING QUERY", flush=True)
+
+        q_exp = Q()
+        for fld in ["filename", "imported_timestamp_str", "file_type__name", "file_format__name"]:
+            q_exp |= Q(**{f"{fld}__icontains": term})
+        return q_exp
 
 
 class ArchiveFileDetailView(DetailView):
