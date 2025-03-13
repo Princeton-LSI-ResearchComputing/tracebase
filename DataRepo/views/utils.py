ecf901ad75c3df219854406c672dfbc702c9e5c0
hepcat72 <hepcat72@gmail.com>
2025-03-13 17:46:40 -0400

    Added a move of get_cookie, which contains an improvement.
    Details:
    
    - get_cookie now decodes the cookie value.

--- DataRepo/views/utils.py
+++ DataRepo/views/utils.py
@@ -1,3 +1,22 @@
+from urllib.parse import unquote
+
+
+def get_cookie(request, cookie_name, cookie_default=None):
+    val = request.COOKIES.get(cookie_name, "")
+    # A cookie value of an empty string should trigger the default value to be applied
+    if val == "" and val != cookie_default:
+        return cookie_default
+    try:
+        # If this is not an encoded string, an error can be thrown.  A regular string can look like an encoded string,
+        # in which case, the return value will be invalid, but those cases will eventually flush out.
+        return unquote(val)
+    except Exception as e:
+        print(
+            f"WARNING: Encountered unencoded cookie: '{cookie_name}' = '{val}'.  {type(e).__name__}: {e}"
+        )
+        return val
+
+
 # See https://docs.djangoproject.com/en/5.1/howto/outputting-csv/#streaming-large-csv-files
 class Echo:
     """A class that implements just the write method of a file-like interface.
