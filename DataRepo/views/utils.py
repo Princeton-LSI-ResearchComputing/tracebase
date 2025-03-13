bc6222a78ff4808a1b3ebb32e4b48ffcd00eefcc
hepcat72 <hepcat72@gmail.com>
2025-03-13 17:35:07 -0400

    Documentation updates and code reorganization.
    Details:
    
    - Moved the javascript cookie methods into their own file and updated the script includes in the base template.
    - Moved widget classes from forms.py into widgets.
    - Moved Echo and ZipBuffer classes into the view utils.
    - Clarified some random documentation and fixed some typos.

--- DataRepo/views/utils.py
+++ DataRepo/views/utils.py
@@ -1,2 +1,36 @@
-def get_cookie(request, cookie_name, cookie_default):
-    return request.COOKIES.get(cookie_name, cookie_default)
+# See https://docs.djangoproject.com/en/5.1/howto/outputting-csv/#streaming-large-csv-files
+class Echo:
+    """A class that implements just the write method of a file-like interface.
+
+    This is intended for use by a csv writer.
+    """
+
+    def write(self, value):
+        return value
+
+
+class ZipBuffer:
+    """A class that implements a zip-file-like interface.
+
+    This is intended for use by a zipfile writer.
+    """
+
+    def __init__(self):
+        self.buf = bytearray()
+
+    def write(self, data):
+        self.buf.extend(data)
+        return len(data)
+
+    def flush(self):
+        pass
+
+    def take(self):
+        buf = self.buf
+        self.buf = bytearray()
+        return bytes(buf)
+
+    def end(self):
+        buf = self.buf
+        self.buf = None
+        return bytes(buf)
