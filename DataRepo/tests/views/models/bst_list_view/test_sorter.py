932ddc4a89cefc770d60bc580afa4e6c9d3d085b
hepcat72 <hepcat72@gmail.com>
2025-03-27 12:58:58 -0400

    Streamlined the BSTSorter code to make it more flexible.
    Details:
    
    - Added a __eq__ override to be able to compare objects
    - Enforced a rule in BSTSorter that a custom field expression (i.e. with a transform) should be accompanied by a custom client sorter and vice versa.  Did this by adding a warning, so it's a soft enforcement.
    - Removed BSTSorter.identity
    - Modified the BSTSorter constructor
      - Changed field argument to field_expression, to replace the need for the transform argument.  Added Combinable and str to the union of types accepted.
      - Added an optional model input, used to set the default client_sorter
      - Documented only supporting a single source field in an expression as a limitation
      - Documented the following assumptions
        - When a Field is provided, the assumed field path is the field's immediate parent.
        - The output type of a field expression is the same type as the field.
    
    Files checked in: DataRepo/tests/views/models/bst_list_view/test_sorter.py DataRepo/views/models/bst_list_view/sorter.py

--- DataRepo/tests/views/models/bst_list_view/test_sorter.py
+++ DataRepo/tests/views/models/bst_list_view/test_sorter.py
@@ -1,75 +1,109 @@
-from django.db.models import CharField, IntegerField
+from django.db.models import CharField, F, IntegerField
 from django.db.models.functions import Lower, Upper
 from django.templatetags.static import static
 from django.test import override_settings
 
-from DataRepo.tests.tracebase_test_case import TracebaseTestCase
+from DataRepo.tests.tracebase_test_case import (
+    TracebaseTestCase,
+    create_test_model,
+)
 from DataRepo.views.models.bst_list_view.sorter import BSTSorter
+
+BSTSTestModel = create_test_model(
+    "BSTSTestModel",
+    {
+        "name": CharField(max_length=255),
+        "value": IntegerField(),
+    },
+)
 
 
 class BSTSorterTests(TracebaseTestCase):
-    def test_init_none(self):
-        s = BSTSorter()
-        self.assertEqual(BSTSorter.identity, s.transform)
-        self.assertEqual(BSTSorter.SORTER_JS_DJANGO, s.sorter)
-        self.assertEqual(BSTSorter.SORTER_JS_ALPHANUMERIC, s.client_sorter)
-
+    @override_settings(DEBUG=True)
+    @TracebaseTestCase.assertNotWarns()
     def test_init_charfield(self):
-        s = BSTSorter(field=CharField())
-        self.assertEqual(Lower, s.transform)
+        s = BSTSorter(CharField(name="name"))
         self.assertEqual(BSTSorter.SORTER_JS_DJANGO, s.sorter)
         self.assertEqual(BSTSorter.SORTER_JS_ALPHANUMERIC, s.client_sorter)
+        self.assertIsInstance(s.sort_expression, Lower)
 
+    @override_settings(DEBUG=True)
+    @TracebaseTestCase.assertNotWarns()
     def test_init_integerfield(self):
-        s = BSTSorter(field=IntegerField())
-        self.assertEqual(BSTSorter.identity, s.transform)
+        s = BSTSorter(IntegerField(name="value"))
         self.assertEqual(BSTSorter.SORTER_JS_DJANGO, s.sorter)
         self.assertEqual(BSTSorter.SORTER_JS_NUMERIC, s.client_sorter)
+        self.assertIsInstance(s.sort_expression, F)
 
-    def test_init_transform(self):
-        s = BSTSorter(transform=Upper)
-        self.assertEqual(Upper, s.transform)
+    @override_settings(DEBUG=True)
+    @TracebaseTestCase.assertNotWarns()
+    def test_init_path_str_and_model(self):
+        s = BSTSorter("value", model=BSTSTestModel)
+        self.assertEqual(BSTSorter.SORTER_JS_DJANGO, s.sorter)
+        self.assertEqual(BSTSorter.SORTER_JS_NUMERIC, s.client_sorter)
+        self.assertIsInstance(s.sort_expression, F)
+
+    @override_settings(DEBUG=True)
+    @TracebaseTestCase.assertNotWarns()
+    def test_init_path_f_and_model(self):
+        s = BSTSorter(F("name"), model=BSTSTestModel)
         self.assertEqual(BSTSorter.SORTER_JS_DJANGO, s.sorter)
         self.assertEqual(BSTSorter.SORTER_JS_ALPHANUMERIC, s.client_sorter)
+        self.assertIsInstance(s.sort_expression, Lower)
 
-    def test_init_sorter_bst(self):
-        s = BSTSorter(client_sorter=BSTSorter.SORTER_BST_NUMERIC)
+    @override_settings(DEBUG=True)
+    def test_init_path_expression_and_model(self):
+        with self.assertWarns(UserWarning) as aw:
+            s = BSTSorter(Upper("name"), model=BSTSTestModel)
+        self.assertEqual(1, len(aw.warnings))
+        self.assertIn(
+            "without a corresponding client_sorter", str(aw.warnings[0].message)
+        )
+        self.assertIn("Unable to select a client_sorter", str(aw.warnings[0].message))
+        self.assertIn(
+            "Server sort may differ from client sort", str(aw.warnings[0].message)
+        )
+        self.assertIn(
+            "client_sorter based on the field type 'CharField'",
+            str(aw.warnings[0].message),
+        )
         self.assertEqual(BSTSorter.SORTER_JS_DJANGO, s.sorter)
-        self.assertEqual(BSTSorter.SORTER_BST_NUMERIC, s.client_sorter)
+        self.assertEqual(BSTSorter.SORTER_JS_ALPHANUMERIC, s.client_sorter)
+        self.assertIsInstance(s.sort_expression, Upper)
+
+    @override_settings(DEBUG=True)
+    @TracebaseTestCase.assertNotWarns()
+    def test_init_path_expression_model_and_clientsorter(self):
+        BSTSorter(Upper("name"), model=BSTSTestModel, client_sorter="upperSorter")
 
     @override_settings(DEBUG=True)
     def test_init_sorter_custom(self):
-        with self.assertWarns(UserWarning):
-            s = BSTSorter(client_sorter="mySorter")
+        with self.assertWarns(UserWarning) as aw:
+            s = BSTSorter("value", client_sorter="mySorter")
+        self.assertEqual(1, len(aw.warnings))
+        self.assertIn(
+            "field_expression (value) supplied without a model.",
+            str(aw.warnings[0].message),
+        )
+        self.assertIn("Unable to determine field type", str(aw.warnings[0].message))
+        self.assertIn(
+            "transform ('Lower') that matches the client_sorter",
+            str(aw.warnings[0].message),
+        )
+        self.assertIn(
+            "Server sort may differ from client sort", str(aw.warnings[0].message)
+        )
+        self.assertIn("Defaulting to expression as-is.", str(aw.warnings[0].message))
         self.assertEqual(BSTSorter.SORTER_JS_DJANGO, s.sorter)
         self.assertEqual("mySorter", s.client_sorter)
+        self.assertIsInstance(s.sort_expression, F)
 
-    def test_init_invalid_transform_object(self):
-        with self.assertRaises(ValueError) as ar:
-            BSTSorter(transform="name")
-        ve = ar.exception
-        self.assertEqual(
-            "transform must be a Combinable, e.g. type Transform.", str(ve)
-        )
-
-    def test_init_invalid_transform_class(self):
-        with self.assertRaises(ValueError) as ar:
-            BSTSorter(transform=str)
-        ve = ar.exception
-        self.assertEqual(
-            "transform must be a Combinable, e.g. type Transform.", str(ve)
-        )
-
-    def test_identity_transform(self):
-        order_by_val = BSTSorter.identity(Upper("name"))
-        self.assertEqual(Upper("name"), order_by_val)
-
-    def test_identity_str(self):
-        order_by_val = BSTSorter.identity("-name")
-        self.assertEqual("-name", order_by_val)
-
+    @override_settings(DEBUG=True)
+    @TracebaseTestCase.assertNotWarns()
     def test_str(self):
-        self.assertEqual(BSTSorter.SORTER_JS_DJANGO, str(BSTSorter()))
+        self.assertEqual(
+            BSTSorter.SORTER_JS_DJANGO, str(BSTSorter(CharField(name="name")))
+        )
 
     def test_javascript(self):
         self.assertEqual(
@@ -77,16 +111,20 @@ class BSTSorterTests(TracebaseTestCase):
             BSTSorter.javascript,
         )
 
+    @override_settings(DEBUG=True)
+    @TracebaseTestCase.assertNotWarns()
     def test_set_client_mode(self):
-        s = BSTSorter()
+        s = BSTSorter(CharField(name="name"))
         self.assertFalse(s.client_mode)
         s.set_client_mode()
         self.assertTrue(s.client_mode)
         s.set_client_mode(enabled=False)
         self.assertFalse(s.client_mode)
 
+    @override_settings(DEBUG=True)
+    @TracebaseTestCase.assertNotWarns()
     def test_set_server_mode(self):
-        s = BSTSorter()
+        s = BSTSorter(CharField(name="name"))
         s.set_server_mode()
         self.assertFalse(s.client_mode)
         s.set_server_mode(enabled=False)
