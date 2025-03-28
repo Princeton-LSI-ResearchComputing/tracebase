1e8dbcfb93520abeb8c00a16032a7096e3a4abea
Nibal <na1456@princeton.edu>
2025-03-28 14:04:26 -0400

    isort linting

--- DataRepo/tests/views/upload/test_submission.py
+++ DataRepo/tests/views/upload/test_submission.py
@@ -3,8 +3,8 @@ import os
 from copy import deepcopy
 from io import BytesIO
 
-from django.core.management import call_command
 from django.core.exceptions import PermissionDenied
+from django.core.management import call_command
 from django.test import override_settings
 from django.urls import reverse
 
