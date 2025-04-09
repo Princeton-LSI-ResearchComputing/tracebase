c62cdd6681924edb2b7901f049d7ae0a4ccc1aeb
hepcat72 <hepcat72@gmail.com>
2025-04-09 13:51:35 -0400

    Implemented remaining tests for BSTColumnGroup and fixed issues they revealed.
    Details:
    
    - Changed the argument default for asc in BSTBaseColumn so that it could account for an explicit setting to None (thereby being able to handle setting the class-defined default when it was explicitly supplied as None).
    - Improved BSTColumnGroup warnings to make sure they supply suggested fixes, describe the problem scope, etc.
    - Made sure that set_related_model_path(self.related_model_path) is called on each column when the value being supplied is valid, otherwise, any reported errors are not necessarily comprehensive and provide an inaccurate context (suggesting a value was supplied by the user when in fact, a default was supplied).
    - Defaulted BSTColumnGroup's asc instance member to that of the sorter that was created from the controlling column.
    - Removed asc as an argument to the BSTColumnGroup constructor because it was redundant with that defined in the controlling column, and can be confusing when related to the other columns' defaults.
    - Added a many-related check to the BSTManyRelatedColumn constructor.
    - Changed the argument default for asc in BSTManyRelatedColumn so that it could account for an explicit setting to None (thereby being able to handle setting the class-defined default when it was explicitly supplied as None).
    - Added method is_many_related_to_parent to the model utilities to correspond with the behavior of field_path_to_model_path when many_related is supplied as True.
      - Changed calls to is_many_related_to_root to is_many_related_to_parent in BSTColumnGroup
      - Note that singly-related models (to their parents) are considered as many-related to the root model if the path passes through a many-related relation at any point.
    
    Files checked in: DataRepo/models/utilities.py DataRepo/tests/models/test_utilities.py DataRepo/tests/views/models/bst_list_view/column/test_many_related_group.py DataRepo/views/models/bst_list_view/column/many_related_field.py DataRepo/views/models/bst_list_view/column/many_related_group.py DataRepo/views/models/bst_list_view/column/sorter/base.py

--- DataRepo/views/models/bst_list_view/column/many_related_group.py
+++ DataRepo/views/models/bst_list_view/column/many_related_group.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from typing import Optional
+from typing import Optional, cast
 
-from DataRepo.models.utilities import field_path_to_model_path, is_many_related_to_root
+from DataRepo.models.utilities import (
+    field_path_to_model_path,
+    is_many_related_to_parent,
+)
 from DataRepo.views.models.bst_list_view.column.many_related_field import (
     BSTManyRelatedColumn,
 )
-from DataRepo.views.models.bst_list_view.column.sorter.many_related_field import BSTManyRelatedSorter
+from DataRepo.views.models.bst_list_view.column.sorter.many_related_field import (
+    BSTManyRelatedSorter,
+)
 
 
 class BSTColumnGroup:
@@ -25,7 +30,6 @@ class BSTColumnGroup:
         self,
         *columns: BSTManyRelatedColumn,
         initial: Optional[str] = None,
-        asc: Optional[bool] = None,
         related_model_path: Optional[str] = None,
     ):
         """Constructor.
@@ -49,8 +53,6 @@ class BSTColumnGroup:
             *columns (BSTManyRelatedColumn): Minimum of 2 BSTManyRelatedColumn objects.
             initial (Optional[str]) [columns[0].name]: The name of the BSTManyRelatedColumn in columns that the sort
                 will be based on.
-            asc (Optional[bool]) [auto]: The initial sort direction (ascending or descending).  The default is that of
-                the initial column.
             related_model_path (Optional[str]) [auto]: The many-related model path common among all of the columns.  The
                 default is based on the last many-related model path of the first column.  You should only need to
                 provide this if a column's field_path traverses mutliple many-related models and the last one in the
@@ -68,42 +70,54 @@ class BSTColumnGroup:
         """
         # Initial columns checks required for defaults set below
         not_bstmrcs = [
-            f"column {i + 1}: {type(c).__name__}"
+            f"column {i + 1}, '{c.name}': {type(c).__name__}"
             for i, c in enumerate(columns)
             if not isinstance(c, BSTManyRelatedColumn)
         ]
         if len(not_bstmrcs) > 0:
+            nlt = "\n\t"
             raise TypeError(
-                f"Invalid columns.  {len(not_bstmrcs)} of the {len(columns)} columns are the wrong type: {not_bstmrcs}."
+                f"Invalid columns.  {len(not_bstmrcs)} of the {len(columns)} columns are the wrong type:\n"
+                f"\t{nlt.join(not_bstmrcs)}\nColumn objects must be BSTManyRelatedColumn instances."
             )
         if len(columns) < 2:
-            raise ValueError(f"Invalid columns.  There must be more than 1.  Supplied: {len(columns)}.")
+            raise ValueError(
+                f"Invalid columns.  There must be more than 1.  Supplied: {len(columns)}."
+            )
 
         # Defaults
         self.columns = columns
-        self.asc = asc
-        self.initial = (initial if initial is not None else columns[0].name)
+        self.initial = initial if initial is not None else columns[0].name
         self.model = columns[0].model
 
-        self.related_model_path = (
-            related_model_path
-            if related_model_path is not None
-            else field_path_to_model_path(columns[0].model, columns[0].related_model_path)
+        # We are guaranteed to have a non-None related_model_path, because BSTManyRelatedColumn requires one, so we can
+        # cast the result
+        self.related_model_path = cast(
+            str,
+            (
+                related_model_path
+                if related_model_path is not None
+                else field_path_to_model_path(
+                    columns[0].model, columns[0].related_model_path, many_related=True
+                )
+            ),
         )
 
         self.sorter: BSTManyRelatedSorter
         self.controlling_column: BSTManyRelatedColumn
         for c in columns:
-            if c == initial:
+            if c == self.initial:
                 self.controlling_column = c
-                self.sorter = c.create_sorter(asc=asc)
+                self.sorter = c.create_sorter()
                 break
         else:
             nlt = "\n\t"
             raise ValueError(
-                f"Initial column '{initial}' does not match any of the supplied columns:{nlt}"
+                f"Initial column '{self.initial}' does not match any of the supplied columns:{nlt}"
                 f"{nlt.join([c.name for c in columns])}"
             )
+
+        self.asc = self.controlling_column.sorter.asc
 
         # Check that all columns have the same root model
         not_common_models = [
@@ -114,7 +128,7 @@ class BSTColumnGroup:
         if len(not_common_models) > 0:
             raise ValueError(
                 f"Invalid columns.  {len(not_common_models)} of the {len(columns)} columns do not have the same root "
-                f"model '{self.model}' as the first column: {not_common_models}."
+                f"model '{self.model.__name__}' as the first column: {not_common_models}."
             )
 
         # Check that each column's field_path starts with the same many-related model
@@ -123,7 +137,8 @@ class BSTColumnGroup:
         for c in columns:
             if not c.field_path.startswith(self.related_model_path):
                 not_common_mr_models.append(c.field_path)
-            c.set_related_model_path(self.related_model_path)
+            else:
+                c.set_related_model_path(self.related_model_path)
         if len(not_common_mr_models) > 0:
             nlt = "\n\t"
             raise ValueError(
@@ -135,19 +150,35 @@ class BSTColumnGroup:
             )
 
         # Make sure that self.related_model_path is many-related
-        if not is_many_related_to_root(self.related_model_path, self.model):
+        if not is_many_related_to_parent(self.related_model_path, self.model):
+            try:
+                mm_model = field_path_to_model_path(
+                    self.model, self.controlling_column.field_path, many_related=True
+                )
+                suggestion = (
+                    f"Try setting '{mm_model}', which leads to a many-related model."
+                )
+            except Exception as e:
+                if isinstance(e, ValueError) and "No many-related model" in str(e):
+                    suggestion = "Supply columns that contain a many-related model to the model root."
+                else:
+                    suggestion = f"There was a problem trying to find a many-related model to suggest: {e}"
             raise ValueError(
                 f"The related_model_path '{self.related_model_path}' is not many-related to the root model "
-                f"'{self.model}'."
+                f"'{self.model.__name__}'.  {suggestion}"
             )
 
         # Check for duplicate column names
-        seen = defaultdict(int)
+        seen: dict = defaultdict(int)
         for c in columns:
             seen[c.name] += 1
-        dupes = [k for k in seen.keys() if seen[k] > 1]
-        if len(dupes) > 0:
-            raise ValueError(f"Duplicate column names not allowed: {dupes}")
+        dupes = dict((k, v) for k, v in seen.items() if v > 1)
+        if len(dupes.keys()) > 0:
+            nlt = "\n\t"
+            raise ValueError(
+                f"Duplicate column names not allowed ({len(dupes.keys())} occurrence(s) in {len(columns)} columns):\n"
+                f"\t{nlt.join([f'{k}: {v} occurrences' for k, v in dupes.items()])}"
+            )
 
         # Finally, update all of the columns' sorters
         self.set_sorters()
@@ -186,8 +217,8 @@ class BSTColumnGroup:
             else:
                 nlt = "\n\t"
                 raise ValueError(
-                    f"Column '{column}' does not match any of the columns in this group:{nlt}"
-                    f"{nlt.join([c.name for c in self.columns])}"
+                    f"Column '{column}' does not match any of the columns in this group:\n"
+                    f"\t{nlt.join([c.name for c in self.columns])}"
                 )
         elif asc is not None and asc != self.controlling_column.sorter.asc:
             self.sorter = self.controlling_column.create_sorter(asc=asc)
