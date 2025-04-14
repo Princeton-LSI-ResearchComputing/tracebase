b7db6bb702fdd8884b95bfbc8c2f863dca977915
hepcat72 <hepcat72@gmail.com>
2025-04-14 18:24:09 -0400

    Added initialization of BSTListView.columns and made some changes based on what I learned from the tests.
    Details:
    
    - Added an __eq__ method to BSTColumnGroup
    - Changed BSTColumnGroup.columns from a tuple to a list
    - Added a warning in BSTBaseColumn's __eq__ method when comparing a BSTManyRelatedColumn in a group with one that's not and vice versa, since the group modifies the column.
    - Added a __str__ method in BSTBaseColumn
    - Added an instance member named _in_group to BSTBaseColumn
    - Added methods to BSTListView:
      - init_column_group
      - init_column
      - add_to_column_ordering
    - Fixed a bug in is_many_related_to_parent to handle a "-" preceding a field name
    - Added method: is_related to the model utilities.
    
    Files checked in: DataRepo/models/utilities.py DataRepo/tests/models/test_utilities.py DataRepo/tests/views/models/bst_list_view/test_base.py DataRepo/views/models/bst_list_view/base.py DataRepo/views/models/bst_list_view/column/base.py DataRepo/views/models/bst_list_view/column/many_related_group.py

--- DataRepo/tests/views/models/bst_list_view/test_base.py
+++ DataRepo/tests/views/models/bst_list_view/test_base.py
@@ -1,4 +1,10 @@
-from django.db.models import CharField, ManyToManyField
+from django.db.models import (
+    CASCADE,
+    CharField,
+    Count,
+    ForeignKey,
+    ManyToManyField,
+)
 from django.db.models.functions import Lower
 from django.http import HttpRequest
 from django.test import override_settings
@@ -9,12 +15,18 @@ from DataRepo.tests.tracebase_test_case import (
 )
 from DataRepo.utils.exceptions import DeveloperWarning
 from DataRepo.views.models.bst_list_view.base import BSTListView
+from DataRepo.views.models.bst_list_view.column.annotation import (
+    BSTAnnotColumn,
+)
 from DataRepo.views.models.bst_list_view.column.field import BSTColumn
 from DataRepo.views.models.bst_list_view.column.many_related_field import (
     BSTManyRelatedColumn,
 )
 from DataRepo.views.models.bst_list_view.column.many_related_group import (
     BSTColumnGroup,
+)
+from DataRepo.views.models.bst_list_view.column.related_field import (
+    BSTRelatedColumn,
 )
 
 BSTLVStudyTestModel = create_test_model(
@@ -40,12 +52,31 @@ BSTLVAnimalTestModel = create_test_model(
         "studies": ManyToManyField(
             to="loader.BSTLVStudyTestModel", related_name="animals"
         ),
+        "treatment": ForeignKey(
+            to="loader.BSTLVTreatmentTestModel",
+            related_name="animals",
+            on_delete=CASCADE,
+        ),
     },
+)
+
+BSTLVTreatmentTestModel = create_test_model(
+    "BSTLVTreatmentTestModel",
+    {"name": CharField(unique=True), "desc": CharField()},
 )
 
 
 class StudyLV(BSTListView):
     model = BSTLVStudyTestModel
+
+
+class AnimalLV(BSTListView):
+    model = BSTLVAnimalTestModel
+
+
+class AnimalNoStudiesLV(BSTListView):
+    model = BSTLVAnimalTestModel
+    exclude = ["id", "studies"]
 
 
 @override_settings(DEBUG=True)
@@ -100,7 +131,14 @@ class BSTListViewTests(TracebaseTestCase):
         self.assertEqual(0, blv.raw_total)
         self.assertEqual([], blv.warnings)
         self.assertEqual({}, blv.column_settings)
-        self.assertEqual({}, blv.columns)
+        self.assertEqual(
+            {
+                "name": BSTColumn("name", BSTLVStudyTestModel),
+                "desc": BSTColumn("desc", BSTLVStudyTestModel),
+                "animals": BSTManyRelatedColumn("animals", BSTLVStudyTestModel),
+            },
+            blv.columns,
+        )
         self.assertEqual({}, blv.groups)
 
     @TracebaseTestCase.assertNotWarns()
@@ -285,3 +323,122 @@ class BSTListViewTests(TracebaseTestCase):
             "field3", blv.prepare_column_kwargs(d4, settings_name="field3")
         )
         self.assertEqual({"visible": False}, d4)
+
+    def test_init_column_ordering(self):
+        slv = StudyLV()
+        # Defaults case (excludes "id", added by create_test_model)
+        slv.column_ordering = []
+        slv.init_column_ordering()
+        self.assertEqual(["name", "desc", "animals"], slv.column_ordering)
+
+        # User added a related column
+        slv.column_settings = {"animals__desc": {}}
+        slv.column_ordering = []
+        slv.init_column_ordering()
+        self.assertEqual(
+            ["name", "desc", "animals", "animals__desc"], slv.column_ordering
+        )
+
+        # User changes exclude (id appears in order defined, added by create_test_model)
+        slv.exclude = ["animals"]
+        slv.column_settings = {}
+        slv.column_ordering = []
+        slv.init_column_ordering()
+        self.assertEqual(["name", "desc", "id"], slv.column_ordering)
+
+    @TracebaseTestCase.assertNotWarns(DeveloperWarning)
+    def test_add_to_column_ordering(self):
+        slv = StudyLV()
+        slv.add_to_column_ordering("animals__desc")
+        self.assertEqual("animals__desc", slv.column_ordering[-1])
+
+        # Should warn and ignore if column is excluded
+        with self.assertWarns(DeveloperWarning) as aw:
+            slv.add_to_column_ordering("id")
+        self.assertEqual(1, len(aw.warnings))
+        self.assertIn(
+            "Ignoring attempt to add an excluded column 'id'",
+            str(aw.warnings[0].message),
+        )
+        self.assertIn(
+            "Override the 'exclude' class attribute to include this column",
+            str(aw.warnings[0].message),
+        )
+        self.assertIn("Current excludes: ['id']", str(aw.warnings[0].message))
+        self.assertNotIn("id", slv.column_ordering)
+
+        # Should not warn if _warn=False (asserted by decorator above)
+        slv.add_to_column_ordering("id", _warn=False)
+
+    def test_init_columns(self):
+        alv = AnimalNoStudiesLV(
+            [
+                {"name": "study_count", "converter": Count("studies")},
+                "treatment__desc",
+                BSTColumnGroup(
+                    BSTManyRelatedColumn("studies__name", BSTLVAnimalTestModel),
+                    BSTManyRelatedColumn("studies__desc", BSTLVAnimalTestModel),
+                ),
+            ]
+        )
+        alv.columns = {}
+        # Re-init the column_settings for study_count, because the forst pass would have removed the name:
+        print(f"settings {alv.column_settings}")
+        alv.init_columns()
+        self.assertDictEqual(
+            {
+                "studies__name": BSTManyRelatedColumn(
+                    "studies__name", BSTLVAnimalTestModel
+                ),
+                "studies__desc": BSTManyRelatedColumn(
+                    "studies__desc", BSTLVAnimalTestModel
+                ),
+                "treatment": BSTRelatedColumn("treatment", BSTLVAnimalTestModel),
+                "treatment__desc": BSTRelatedColumn(
+                    "treatment__desc", BSTLVAnimalTestModel
+                ),
+                "desc": BSTColumn("desc", BSTLVAnimalTestModel),
+                "name": BSTColumn("name", BSTLVAnimalTestModel),
+                "study_count": BSTAnnotColumn("study_count", Count("studies")),
+            },
+            alv.columns,
+        )
+
+    def test_init_column(self):
+        alv = AnimalLV()
+        alv.columns = {}
+        alv.init_column("treatment__desc")
+        self.assertEqual(
+            BSTRelatedColumn("treatment__desc", BSTLVAnimalTestModel),
+            alv.columns["treatment__desc"],
+        )
+        alv.column_settings["study_count"] = {"converter": Count("studies")}
+        alv.init_column("study_count")
+        self.assertEqual(
+            BSTAnnotColumn("study_count", Count("studies")), alv.columns["study_count"]
+        )
+
+    def test_init_column_group(self):
+        alv = AnimalNoStudiesLV()
+        alv.column_settings["studies_group"] = BSTColumnGroup(
+            BSTManyRelatedColumn("studies__name", BSTLVAnimalTestModel),
+            BSTManyRelatedColumn("studies__desc", BSTLVAnimalTestModel),
+        )
+        size_before = len(alv.columns.keys())
+        group = BSTColumnGroup(
+            BSTManyRelatedColumn("studies__name", BSTLVAnimalTestModel),
+            BSTManyRelatedColumn("studies__desc", BSTLVAnimalTestModel),
+        )
+        alv.init_column_group(group)
+        self.assertEqual(size_before + 2, len(alv.columns.keys()))
+        # NOTE: Creating a BSTManyRelatedColumn on the fly will not be equal to the one added to columns, because the
+        # group constructor modifies it (so they sort the same).
+        self.assertEqual(group.columns[0], alv.columns["studies__name"])
+        self.assertEqual(group.columns[1], alv.columns["studies__desc"])
+        self.assertDictEqual(
+            {
+                "studies__name": group,
+                "studies__desc": group,
+            },
+            alv.groups,
+        )
