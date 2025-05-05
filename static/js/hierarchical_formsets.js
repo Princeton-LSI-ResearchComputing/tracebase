Princeton-LSI-ResearchComputing/tracebase

commit a8f0c5c4a91240b00f86f58fd23ecfd7ffbe81a6

Reorganized widgets and templates in preparation for BST List View.  Also fixed a minor javascript bug I found in the hierarchical formsets.
The javascript bug was that the initial value of the ncmp select list for the FCirc template was not getting pre-selected, so if you submitted a search without noticing, it would throw a 500 error.  All I had to do was set the select element's value before sending it into the call to updateNcmpChoices.

static/js/hierarchical_formsets.js
@@ -243,8 +243,8 @@ function addSearchFieldForm (myDiv, query, templateId) {
   }
 
   // Initialize the ncmp choices
-  updateNcmpChoices(fldInitVal, ncmpClone, templateId)
   ncmpClone.value = ncmpInitVal
+  updateNcmpChoices(fldInitVal, ncmpClone, templateId)
 
   // Initialize the units choices
   unitsClone.value = unitsInitVal

commit 5a424fda38e6624ec9b639346d4cb1901dbcdabf

Fixes a bug introduced by commit a8f0c5c which was trying to fix THIS bug.
The problem was that the FCirc format's initial ncmp select list had no default selection.  The fix had been to set the initial value BEFORE updating the choices.  However, the side-effect of that was to always set the first item in the select list.  So whenever a user selected a different value and submited a search, their search parameter for the ncmp select list would be changed.

The actual problem is that the ncmp choices are initially dictated by the first field in the selected template.  That first field in the peak group template is Age (a number), but the first field in the fcirc template is animal (name) (a string).  Therefore, the first item in each ncmp select list ("is") gets the value of "exact" for the number type.  It should be "iexact" for the string type.  The code dynamically updates the choices, but the initial value is stale.

To fix it, I search the ncmp choices after the choices update and if the initial value isn't there, I set the first value.

There has to be a better way to do this, but for now, this works.

Files checked in: static/js/hierarchical_formsets.js

static/js/hierarchical_formsets.js
@@ -243,8 +243,30 @@ function addSearchFieldForm (myDiv, query, templateId) {
   }
 
   // Initialize the ncmp choices
-  ncmpClone.value = ncmpInitVal
   updateNcmpChoices(fldInitVal, ncmpClone, templateId)
+  // We need to update the selected ncmp selection to what was previously selected, but upon the initial load of the
+  // advanced search, the initial ncmp choices are those of the selected template.  Those choices are updated by
+  // updateNcmpChoices, but the ncmpInitVal is NOT.  There is VERY likely a better solution than this check to see if
+  // ncmpInitVal is valid, but for now, this is the fix.
+  // TODO: Figure out how to select an ncmpInitVal that is specific to the template without having to do this loop.
+  // Notes: The above "ncmpInitVal = ncmpClone[0].value" assignment is wrong for the fctemplate, because the default
+  // ncmp choices are based on the selected template, which by default is for PeakGroup Age (a number), which gets the
+  // "is" ncmp value of "exact".  The first field in the FCirc template is Animal (a string), which gets the "is" ncmp
+  // value of "iexact".  This is the problem that the loop below solves: it determines that "exact" isn't among the
+  // choices and defaults to the first choice "iexact".
+  const initFldType = fldTypes[templateId][fldInitVal].type
+  const initChoices = ncmpChoices[initFldType]
+  let initMatch = false
+  for (let i = 0; i < initChoices.length; i++) {
+    if (ncmpInitVal === initChoices[i][0]) {
+      initMatch = true
+      break
+    }
+  }
+  if (!initMatch) {
+    ncmpInitVal = initChoices[0][0]
+  }
+  ncmpClone.value = ncmpInitVal
 
   // Initialize the units choices
   unitsClone.value = unitsInitVal