bf3e348848d74335c67db12643f5c7327372088d
hepcat72 <hepcat72@gmail.com>
2025-05-21 18:29:27 -0400

    Changed the delimiters replacement with a different (closely matching) character in each case and added a suggestion that points out everywhere the change needs to be made.
    This was to address the issue of the user editing in the Study doc Compounds sheet and not in the peak annotation files.
    
    Files checked in: DataRepo/loaders/compounds_loader.py DataRepo/loaders/peak_annotations_loader.py DataRepo/models/compound.py DataRepo/tests/loaders/test_compounds_loader.py DataRepo/tests/loaders/test_peak_annotations_loader.py DataRepo/tests/models/test_compound.py DataRepo/utils/exceptions.py

--- DataRepo/tests/loaders/test_compounds_loader.py
+++ DataRepo/tests/loaders/test_compounds_loader.py
@@ -15,11 +15,28 @@ class CompoundsLoaderTests(TracebaseTestCase):
         )
         cl.load_data()
         # Test fails if either of these raises
-        Compound.objects.get(name="2-hydroxyglutarate")
-        CompoundSynonym.objects.get(name="L-2-hydroxyglutarate")
+        Compound.objects.get(name="2:hydroxyglutarate")
+        CompoundSynonym.objects.get(name="L-2\\hydroxyglutarate")
         self.assertEqual(1, len(cl.aggregated_errors_object.exceptions))
         self.assertTrue(
             cl.aggregated_errors_object.exception_type_exists(ProhibitedCompoundNames)
         )
         self.assertFalse(cl.aggregated_errors_object.exceptions[0].is_error)
         self.assertFalse(cl.aggregated_errors_object.exceptions[0].is_fatal)
+        # Check the suggestion added by the loader
+        self.assertIn(
+            "You may manually edit the compound names",
+            str(cl.aggregated_errors_object.exceptions[0]),
+        )
+        self.assertIn(
+            "with whatever replacement characters you wish",
+            str(cl.aggregated_errors_object.exceptions[0]),
+        )
+        self.assertIn(
+            "do so in both the study doc's Compounds sheet",
+            str(cl.aggregated_errors_object.exceptions[0]),
+        )
+        self.assertIn(
+            "AND in all peak annotation files.",
+            str(cl.aggregated_errors_object.exceptions[0]),
+        )
