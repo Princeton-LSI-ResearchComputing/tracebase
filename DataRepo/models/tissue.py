82f19e7f3b44a990bbb1d5a77e09b88109413ca2
hepcat72 <hepcat72@gmail.com>
2025-05-28 18:47:38 -0400

    Added class methods to encapsulate testing if a string is a serum tissue name and to incorporate Q expressions that require a tissue to be a serum tissue.
    Files checked in: DataRepo/loaders/samples_loader.py DataRepo/models/animal.py DataRepo/models/tissue.py

--- DataRepo/models/tissue.py
+++ DataRepo/models/tissue.py
@@ -1,17 +1,18 @@
-from django.db import models
+from django.db import ProgrammingError
+from django.db.models import AutoField, CharField, Model, Q, TextField
 
 
-class Tissue(models.Model):
+class Tissue(Model):
     detail_name = "tissue_detail"
     SERUM_TISSUE_PREFIX = "serum"
 
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(
+    id = AutoField(primary_key=True)
+    name = CharField(
         max_length=256,
         unique=True,
         help_text='The laboratory standardized name for this tissue type (e.g. "serum", "brain", "liver").',
     )
-    description = models.TextField(
+    description = TextField(
         help_text="Description of this tissue type.",
     )
 
@@ -25,6 +26,27 @@ class Tissue(models.Model):
 
     def is_serum(self):
         return self.name.startswith(Tissue.SERUM_TISSUE_PREFIX)
+
+    @classmethod
+    def name_is_serum(cls, tissue_name: str):
+        return tissue_name.startswith(Tissue.SERUM_TISSUE_PREFIX)
+
+    @classmethod
+    def serum_q_expression(cls, field_path: str):
+        """Return a Q expression to check that the tissue name in the supplied field path is a serum tissue.
+
+        Args:
+            field_path (str): E.g. 'animals__samples__tissue__name'.  Must end in 'name'
+        Exceptions:
+            ProgrammingError - when the field_path is invalid.
+        Returns:
+            (Q)
+        """
+        if not field_path.endswith("name"):
+            raise ProgrammingError(
+                f"field_path '{field_path}' must end with 'name' (the tissue's name field)."
+            )
+        return Q(**{f"{field_path}__istartswith": Tissue.SERUM_TISSUE_PREFIX})
 
     def get_absolute_url(self):
         """Get the URL to the detail page.
