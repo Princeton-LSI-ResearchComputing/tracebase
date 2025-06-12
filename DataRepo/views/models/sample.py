0927c36f334b33f6f538858eab956d3d4ba37053
hepcat72 <hepcat72@gmail.com>
2025-06-12 18:20:25 -0400

    Using the sample BSTListView as a trial, I fixed various javascript errors not covered by the tests.
    Details:
    
    - Replaced `SampleListView(ListView)` with `SampleListView(BSTListView)` containing only the model class attribute.
    - Fixed a bunch of script paths in various view classes.
    - Added a pylint disable to a couple lines that had false positives.
    - Fixed a javascript variable name from "cookie_resets" to "cookieResets".
    - Used the static tag to include script sources and loaded the static tags library in the appropriate templates.
    - Moved the call to setCollapse BELOW the initialization of the bootstrap table, since it looks for HTML elements added by bootstrap table.
    
    Files checked in: DataRepo/static/js/bst/list_view.js DataRepo/templates/models/bst/paginator.html DataRepo/templates/models/bst/scripts.html DataRepo/tests/templates/models/bst/test_list_view.py DataRepo/views/models/bst/client_interface.py DataRepo/views/models/bst/column/filterer/base.py DataRepo/views/models/sample.py

--- DataRepo/views/models/sample.py
+++ DataRepo/views/models/sample.py
@@ -1,33 +1,11 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView
 
 from DataRepo.models import Sample
-from DataRepo.utils import QuerysetToPandasDataFrame as qs2df
+from DataRepo.views.models.bst.list_view import BSTListView
 
 
-class SampleListView(ListView):
-    """
-    Generic class-based view for a list of samples
-    "model = Sample" is shorthand for queryset = Sample.objects.all()
-    use queryset syntax for sample list with or without filtering
-    """
-
-    # return all samples without query filter
-    queryset = Sample.objects.all()
-    context_object_name = "sample_list"
-    template_name = "models/sample/sample_list.html"
-    ordering = ["animal_id", "name"]
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get the context
-        context = super().get_context_data(**kwargs)
-        #  add data from the DataFrame to the context
-        all_anim_msrun_df = qs2df.get_animal_msrun_all_df()
-
-        # convert DataFrame to a list of dictionary
-        data = qs2df.df_to_list_of_dict(all_anim_msrun_df)
-
-        context["df"] = data
-        return context
+class SampleListView(BSTListView):
+    model = Sample
 
 
 class SampleDetailView(DetailView):
