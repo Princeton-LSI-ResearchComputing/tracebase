6b0f2a7f73c8d98e0c2972bcf9aa4fae0f9cc2cd
hepcat72 <hepcat72@gmail.com>
2026-01-15 13:57:57 -0500

    More debug prints.
    Files checked in: DataRepo/loaders/msruns_loader.py
    [skip ci]

--- DataRepo/loaders/msruns_loader.py
+++ DataRepo/loaders/msruns_loader.py
@@ -2816,6 +2816,7 @@ class MSRunsLoader(TableLoader):
                     ) == os.path.normpath(dir):
                         return multiple_mzxml_dict[dir][0], False
 
+            print(f"WWW multiple_mzxml_dict: {multiple_mzxml_dict}")
             if multiple_mzxml_dict is not None and mzxml_string_dir != "":
                 # Check for an exact match
                 for dir in multiple_mzxml_dict.keys():
@@ -2823,7 +2824,9 @@ class MSRunsLoader(TableLoader):
                     if len(multiple_mzxml_dict[dir]) == 1 and os.path.normpath(
                         mzxml_string_dir
                     ) == os.path.normpath(dir):
+                        print("ZZZ MATCH!")
                         return multiple_mzxml_dict[dir][0], False
+                print("ZZZ THERE WAS NO MATCH!")
 
         # If we have a sample_header, that trumps any mzxml we might match using the sample name
         if multiple_mzxml_dict is None and sample_header is not None:
