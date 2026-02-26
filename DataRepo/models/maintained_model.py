9295193ae63fd1fa3468720016c5adc65f19adb2
hepcat72 <hepcat72@gmail.com>
2026-02-26 14:32:48 -0500

    Fixed a minor reporting issue about changes resulting from auto-updates (and tweaked the wording to make it more consistent).
    Files checked in: DataRepo/models/maintained_model.py

--- DataRepo/models/maintained_model.py
+++ DataRepo/models/maintained_model.py
@@ -1672,8 +1672,9 @@ class MaintainedModel(Model):
         the current filter conditions.  One exception of the refresh, is if performing a mass auto-update, in which
         case the filters the were in effect during buffering are used.
         """
-        changed = False
+        anything_changed = False
         for updater_dict in self.get_my_updaters():
+            changed = False
             update_fld = updater_dict["update_field"]
             update_label = updater_dict["update_label"]
 
@@ -1742,6 +1743,7 @@ class MaintainedModel(Model):
                 setattr(self, update_fld, new_val)
 
                 if old_val != new_val:
+                    anything_changed = True
                     changed = True
 
                 # Report the auto-update
@@ -1750,8 +1752,8 @@ class MaintainedModel(Model):
 
                 if changed:
                     print(
-                        f"Auto-updated {self.__class__.__name__}.{update_fld} in {self.__class__.__name__}.{self.pk} "
-                        f"using {update_fun.__qualname__} from [{old_val}] to [{new_val}]."
+                        f"Auto-update of {self.__class__.__name__}.{update_fld} in {self.__class__.__name__}.{self.pk} "
+                        f"using {update_fun.__qualname__} resulted in a change from [{old_val}] to [{new_val}]."
                     )
                 else:
                     print(
@@ -1759,7 +1761,7 @@ class MaintainedModel(Model):
                         f"using {update_fun.__qualname__} resulted in the same value: [{new_val}]."
                     )
 
-        return changed
+        return anything_changed
 
     def get_record_signature(self):
         if self.pk is None:
